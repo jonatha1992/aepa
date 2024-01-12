429a8ea8e589faafa079d442cbe637209c12579c
ctrl1758 <franco.more1758@gmail.com>
2024-01-12 16:46:22 -0300

    se logra dejar los card responsivos y con estilos modo oscuro

--- src/components/Curso.jsx
+++ src/components/Curso.jsx
@@ -1,40 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "../css/curso.css";
-function Curso({ imageSource, title, text, url }) {
+function Curso({ title, image, description }) {
+  console.log(title, image, description);
   return (
     <div className="cardF text-center animate__animated animate__fadeInUp">
-      <div className="container-img-card">
+      <div className="container-img-card col-4">
         <div className="overflow">
-          <img src={imageSource} alt="a wallpaper" className="card-img-top" />
+          <img src={image} alt="a wallpaper" className="card-img-top" />
         </div>
       </div>
-      <div className="card-body text-light">
+      <div className="card-body text-light col-8">
         <h4 className="card-title">{title}</h4>
-        <p className="card-text text-secondary">
-          {text
-            ? text
-            : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam deserunt fuga accusantium excepturi quia, voluptates obcaecati nam in voluptas perferendis velit harum dignissimos quasi ex? Tempore repellat quo doloribus magnam."}
-        </p>
+        <div className="container-text">
+          <p className="card-text text-secondary">{description}</p>
+        </div>
         <div className="container-button-next">
-          <a
-            href={url ? url : "#!"}
-            target="_blank"
-            className="btn btn-outline-secondary border-0"
-            rel="noreferrer"
-          >
-            Go to {title}
-          </a>
+          <button>INSCRIBIRME</button>
         </div>
       </div>
     </div>
   );
 }
-Curso.propTypes = {
-  title: PropTypes.string.isRequired,
-  text: PropTypes.string,
-  url: PropTypes.string,
-  imageSource: PropTypes.string,
-};
 
 export default Curso;
--- src/components/Cursos.jsx
+++ src/components/Cursos.jsx
@@ -18,11 +18,42 @@ function Cursos() {
   }, []);
 
   return (
-    <div className="mt-5 container d-flex justify-content-center align-items-center h-100">
-      <div className=" row">
-        {cursos.map(({ title, image, url, id }) => (
-          <div className="col-md-12" key={id}>
-            <Curso imageSource={image} title={title} url={url} />
+    <div
+      className="mt-5 container  justify-content-center align-items-center h-100"
+      style={{ background: "#1f2020" }}
+    >
+      <div className="curso-grid">
+        {cursos.map((curso, index) => (
+          <div key={index} className="curso-item">
+            <div className="col-12 col-md-4">
+              <img
+                src={curso.image}
+                alt={curso.title}
+                className="curso-image"
+              />
+            </div>
+
+            <div className="curso-details col-12 col-md-8">
+              <h3 className="curso-title">{curso.title}</h3>
+              <div className="container-text">
+                <p className="curso-description">{curso.description}</p>
+              </div>
+              <div className="container-button-next">
+                <button className="btn-inscribirme">
+                  Inscribirme
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="30"
+                    height="30"
+                    fill="currentColor"
+                    className="bi bi-forward"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M9.502 5.513a.144.144 0 0 0-.202.134V6.65a.5.5 0 0 1-.5.5H2.5v2.9h6.3a.5.5 0 0 1 .5.5v1.003c0 .108.11.176.202.134l3.984-2.933.042-.028a.147.147 0 0 0 0-.252l-.042-.028zM8.3 5.647a1.144 1.144 0 0 1 1.767-.96l3.994 2.94a1.147 1.147 0 0 1 0 1.946l-3.994 2.94a1.144 1.144 0 0 1-1.767-.96v-.503H2a.5.5 0 0 1-.5-.5v-3.9a.5.5 0 0 1 .5-.5h6.3z" />
+                  </svg>
+                </button>
+              </div>
+            </div>
           </div>
         ))}
       </div>
