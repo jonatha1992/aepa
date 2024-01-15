5b99d00a0b403c25f217fe3be32e5e17516b8c44
jonathan correa <jonathancorrea291292@hotmail.com>
2024-01-15 14:14:19 -0300

    CONFIRMAR CAMBIOS DE CONTACT

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -13,15 +13,17 @@ export function Alert({ message }) {
     <>
       {visible && (
         <div 
-          className="alert alert-danger alert-dismissible fade show"
+          className="alert alert-danger alert-dismissible fade show mt-2 p-2 align-items-center"
           role="alert"
+          
         >
-          <span>{message}</span>
+          <span className='p-2'>{message}</span>
           <button
             type="button"
-            className="btn-close"
+            className="btn-close "
             aria-label="Close"
             onClick={handleClose}  
+            style={{padding: '0.70rem'}}
           />
         </div>  
       )}
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,60 +1,39 @@
-// Importa las dependencias necesarias
-import { useState } from "react";
-// import { useAuth } from "../context/AuthContext";
-// import { useNavigate } from "react-router-dom";
+import { Formik, Field, Form, ErrorMessage } from "formik";
 import { Alert } from "../components/Alert";
-// import { agregarUser } from "../controllers/controllerUser";
+
 import logo from "../assets/logo-aepa.png";
 
-const Contact = () => {
-    const [newUser, setnewUser] = useState({
-        displayName: "",
-        telephone: "",
-        email: "",
-        message: "",
-    });
-    const [Error, setError] = useState(null);
-    // const { signup } = useAuth();
-    // const navigate = useNavigate();
+const countries = [
+    { label: " (+1)", value: "+1" },
+    { label: " (+44)", value: "+44" },
+    { label: "Argentina (+54)", value: "+54" },
+    // Add more countries as needed
+];
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setnewUser({
-            ...newUser,
-            [name]: value,
-        });
+const Contact = () => {
+    const validateForm = (values) => {
+        const errors = {};
+
+        if (!values.displayName) {
+            errors.displayName = "El nombre es obligatorio";
+        } else if (!values.telephone) {
+            errors.telephone = "El numero es obligatorio";
+        } else if (!values.email) {
+            errors.email = "El correo es obligatorio";
+        } else if (!values.mensaje) {
+            errors.mensaje = "El mensaje es obligatorio";
+        }
+
+        return errors;
     };
 
-    // Manejador de envío del formulario
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setError("");
-        if (
-            newUser.displayName.trim() === "" ||
-            newUser.telephone.trim() === "" ||
-            newUser.email.trim() === "" ||
-            newUser.mensaje.trim() === ""
-        ) {
-            setError("Todos los campos son obligatorios");
-            return;
-        }
+    const handleSubmit = (values, { setSubmitting, resetForm }) => {
+        // Tu lógica de envío del formulario aquí
+        console.log("Formulario enviado:", values);
 
-        try {
-            // Add your form submission logic here
-            console.log("MANDAR MENSAJE USER A WHATSAP");
-        } catch (error) {
-            // Handle authentication errors
-            if (
-                error.code === "auth/internal-error" ||
-                error.code === "auth/invalid-email"
-            ) {
-                setError("Correo Invalido");
-            } else if (error.code === "auth/email-already-in-use") {
-                setError("Ya existe el email registrado");
-            } else if (error.code === "auth/weak-password") {
-                setError("La contraseña debe tener al menos 6 caracteres");
-            }
-        }
+        // Puedes resetear el formulario después de un envío exitoso
+        resetForm();
+        setSubmitting(false);
     };
 
     return (
@@ -67,97 +46,135 @@ const Contact = () => {
                     borderRadius: "1rem",
                 }}
             >
-                {/* {Error && <Alert message={Error} />} */}
                 <div className="row justify-content-between">
                     <div className="sign-in-container col-lg-4 ">
-                        <div className="sign-in-container m-4 col-8"></div>
-                        <form
-                            className="mx-auto col-lg-12"
+                        <div className="sign-in-container m-4 col-8 "></div>
+                        <Formik
+                            initialValues={{
+                                displayName: "",
+                                telephone: "",
+                                email: "",
+                                mensaje: "",
+                                country: countries[0].value,
+                            }}
+                            validate={validateForm}
                             onSubmit={handleSubmit}
                         >
-                            <h1
-                                className="text-center mb-4"
-                                style={{ color: "white" }}
-                            >
-                                Contacto
-                            </h1>
-                            <div className="form-floating mb-3">
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    placeholder="Enter your name"
-                                    name="displayName"
-                                    value={newUser.displayName}
-                                    onChange={handleChange}
-                                />
-                                <label>Nombre Completo</label>
-                            </div>
-                            <div className="form-floating mb-3">
-                                <input
-                                    type="number"
-                                    className="form-control"
-                                    placeholder="Enter your telephone"
-                                    name="telephone"
-                                    value={newUser.telephone}
-                                    onChange={handleChange}
-                                />
-                                <label>Celular</label>
-                            </div>
-                            <div className="form-floating mb-3">
-                                <input
-                                    type="email"
-                                    className="form-control"
-                                    placeholder="Enter your email"
-                                    name="email"
-                                    value={newUser.email}
-                                    onChange={handleChange}
-                                />
-                                <label>Email</label>
-                            </div>
+                            <Form className="mx-auto col-lg-12 ">
+                                <h1
+                                    className="text-center mb-4"
+                                    style={{ color: "white" }}
+                                >
+                                    Contacto
+                                </h1>
+                                <div className="form-floating mb-3">
+                                    <Field
+                                        type="text"
+                                        className="form-control"
+                                        placeholder="Enter your name"
+                                        name="displayName"
+                                    />
+                                    <label>Nombre Completo</label>
+                                    <ErrorMessage
+                                        name="displayName"
+                                        component={(props) => (
+                                            <Alert message={props.children} />
+                                        )}
+                                    />
+                                </div>
+                                <div className=" d-flex justify-content-between mb-3">
+                                    <div className="form-floating col-4 ">
+                                        <Field
+                                            as="select"
+                                            name="country"
+                                            className="form-select"
+                                        >
+                                            {countries.map((country) => (
+                                                <option
+                                                    key={country.value}
+                                                    value={country.value}
+                                                >
+                                                    {country.label}
+                                                </option>
+                                            ))}
+                                        </Field>
+                                        <label htmlFor="country">Codigo</label>
+                                        <ErrorMessage
+                                            name="country"
+                                            component="div"
+                                            className="error"
+                                        />
+                                    </div>
+                                    <div className="form-floating col-7">
+                                        <Field
+                                            type="tel"
+                                            className="form-control"
+                                            placeholder="Ingresa tu teléfono"
+                                            name="telephone"
+                                        />
+                                        <label htmlFor="telephone">
+                                            Número sin prefijo
+                                        </label>
+                                        <ErrorMessage
+                                            name="telephone"
+                                            component={(props) => (
+                                                <Alert
+                                                    message={props.children}
+                                                />
+                                            )}
+                                        />
+                                    </div>
+                                </div>
+                                <div className="form-floating mb-3">
+                                    <Field
+                                        type="text"
+                                        className="form-control"
+                                        name="email"
+                                        placeholder="Ingrese your email"
+                                    />
+                                    <label>Email</label>
+                                    <ErrorMessage
+                                        name="email"
+                                        component={(props) => (
+                                            <Alert message={props.children} />
+                                        )}
+                                    />
+                                </div>
+                                <div className="form-floating mb-3">
+                                    <Field
+                                        as="textarea"  // Usar "textarea" en lugar de "text-area"
+                                        name="mensaje"
+                                        className="form-control"
+                                        placeholder="Ingrese el mensaje"
+                                    />
+                                    <label>Mensaje</label>
+                                    <ErrorMessage
+                                        name="mensaje"
+                                        component={(props) => (
+                                            <Alert message={props.children} />
+                                        )}
+                                    />
+                                </div>
+                                k
 
-                            <div className="form-floating mb-4">
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    name="mensaje"
-                                    value={newUser.mensaje}
-                                    onChange={handleChange}
-                                />
-                                <label>Mensaje</label>
-                            </div>
-                            <button className="btn btn-primary btn-lg col-5 ">
-                                Enviar
-                            </button>
-                        </form>
+                                <button
+                                    className="btn btn-primary btn-block w-100 mb-1"
+                                    type="submit"
+                                >
+                                    Enviar
+                                </button>
+                            </Form>
+                        </Formik>
                     </div>
                     <div className="container col-lg-2 ">
                         <img className="img-fluid" src={logo} alt="" />
                     </div>
                     <div className="container col-lg-6 ">
-                        <div>
-                            <h3 className="text-center text-bg-light">Redes</h3>
-                            <ul className="list-unstyled text-bg-light">
-                                <li>AEPA FACEBOOK</li>
-                                <li>AEPA INSTAGRAM</li>
-                                <li>AEPA AEPA2023</li>
-                            </ul>
-                        </div>
-                        <div>
-                            <h3 className="text-center text-bg-light">
-                                Asesorimiento
-                            </h3>
-                            <ul className="list-unstyled text-bg-light">
-                                <li>
-                                    <i src={logo}></i>Secretaria@hotmail.com
-                                </li>
-                                <li>011-4023-2792</li>
-                            </ul>
-                        </div>
+                        {/* Resto del contenido */}
                     </div>
                 </div>
             </div>
         </>
     );
 };
-
 export default Contact;
