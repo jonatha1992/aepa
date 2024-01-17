363c90cff5c208280115a1f7ab75e5564c6647fc
jonathan correa <jonathancorrea291292@hotmail.com>
2024-01-17 11:31:13 -0300

    confirmar

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,8 +3,7 @@ import { Error } from "../components/Error.jsx";
 import { FaWhatsapp, FaFacebook, FaInstagram } from "react-icons/fa"; // Importng all icons for simplicity
 import { FiMail } from "react-icons/fi"; // Importing all icons for simplicity
 import { validarEmail, validarNumeroTelefono } from "../security/Validacion.js";
-
-
+import "../css/contact.css"; 
 const countries = [
   { label: "AR (+54)", value: "+54" },
   { label: "USA (+1)", value: "+1" },
@@ -40,14 +39,10 @@ const Contact = () => {
     setSubmitting(false);
   };
 
-
   return (
     <>
       <div
-        className="container "
-        style={{
-          marginTop: "1rem",
-        }}
+        className=" background-contact  "
       >
         <div className="row justify-content-center ">
           <div className="sign-in-container col-lg-6 ">
@@ -65,8 +60,8 @@ const Contact = () => {
               validateOnSubmit={true}
               validateOnBlur={true}
             >
-              <Form className="mx-auto col-lg-9 col-sm-12 text-secondary fs-6 ">
-                <h1 className="text-center h1 mb-2 text-secondary">Contacto</h1>
+              <Form className="mx-auto col-lg-9 col-sm-12 text-black fs-6 ">
+                <h1 className="text-center h1 mb-2 text-black">Contacto</h1>
                 <div className="form-floating mb-3  ">
                   <Field
                     type="text"
@@ -141,9 +136,9 @@ const Contact = () => {
               </Form>
             </Formik>
           </div>
-          <div className="lg-6 mt-5  container row col-lg-6 p- text-center text-secondary d-flex  align-items-center"
+          <div className="lg-6 mt-5  container row col-lg-6 p- text-center text-black d-flex  align-items-center"
           >
-            <div className="col-lg-12  ">
+            <div className="col-lg-12 text-black ">
               <h3 className="h2">¿Necesitas Ayuda? </h3>
               <ul className="list-unstyled mt-3">
                 <li className="  align-items-center mb-3">
@@ -152,27 +147,29 @@ const Contact = () => {
                     href="https://wa.me/5491140232792"
                     target="_blank"
                     rel="noopener noreferrer"
+                    className="text-black fw-bold"
                   >
                     (+54) 9 11 4023-2792
                   </a>
                 </li>
-                <li className="  align-items-center mb-3">
+                <li className="  align-items-center mb-3 ">
                   <FiMail size={25} className="me-2 " />
-                  <a href="mailto:secretaria@aepa.com.ar">
+                  <a className="text-black fw-bold" href="mailto:secretaria@aepa.com.ar">
                     secretaria@aepa.com.ar
                   </a>
                 </li>
               </ul>
             </div>
             <div className="col-lg-12 mt-3 ">
-              <h3 className="h2">Seguinos en las Redes</h3>
+              <h3 className="h2 mb-3">Seguinos en las Redes</h3>
               <ul className="list-unstyled">
                 <li className="  align-items-center mb-3">
-                  <FaFacebook size={25} className="me-2 text-primary" />
+                  <FaFacebook size={25} className="me-2  text-black " />
                   <a
                     href="https://www.facebook.com/profile.php?id=61551020911888"
                     target="_blank"
                     rel="noopener noreferrer"
+                    className="text-black  fw-bold fw-bold"
                   >
                     Facebook
                   </a>
@@ -189,11 +186,11 @@ const Contact = () => {
                   >
                     <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z" />
                   </svg>{" "}
-                  <a href="https://www.tiktok.com/@aepa2023">Ticktok</a>
+                  <a className="text-black fw-bold" href="https://www.tiktok.com/@aepa2023">Ticktok</a>
                 </li>
                 <li className="  align-items-center mb-3">
                   <FaInstagram size={25} className="me-2" />
-                  <a href="https://www.instagram.com/aepa318/">Instagram</a>
+                  <a className="text-black fw-bold" href="https://www.instagram.com/aepa318/">Instagram</a>
                 </li>
               </ul>
             </div>
