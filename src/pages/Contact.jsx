82cdb7104a0b280b0d99417683e1d18b3b740db3
ctrl1758 <franco.more1758@gmail.com>
2024-01-17 23:59:52 -0300

    arreglo el formik contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,9 @@
- import { Formik, Field, Form, ErrorMessage  } from "formik";
+import { Formik, Field, Form, ErrorMessage } from "formik";
 import { Error } from "../components/Error.jsx";
 import { Alert } from "../components/Alert.jsx";
 import { Redes } from "../components/Redes.jsx";
 import "../css/contact.css"; // Asegúrate de tener un archivo CSS para estilos adicionales
-import * as Yup from 'yup';
+import * as Yup from "yup";
 
 const countries = [
   { label: "AR (+54)", value: "+54" },
@@ -14,7 +14,9 @@ const countries = [
 const validationSchema = Yup.object().shape({
   displayName: Yup.string().required("El nombre es obligatorio"),
   telephone: Yup.string().required("El número es obligatorio"),
-  email: Yup.string().email("El correo no es válido").required("El correo es obligatorio"),
+  email: Yup.string()
+    .email("El correo no es válido")
+    .required("El correo es obligatorio"),
   mensaje: Yup.string().required("El mensaje es obligatorio"),
 });
 
@@ -26,12 +28,9 @@ const Contact = () => {
     resetForm();
   };
 
-
   return (
     <>
-      <div
-        className="container"
-      >
+      <div className="container">
         <div className="row justify-content-center ">
           <div className="sign-in-container col-lg-6 ">
             <div className="sign-in-container m-4 col-8 "></div>
@@ -46,7 +45,8 @@ const Contact = () => {
               validationSchema={validationSchema}
               onSubmit={handleSubmit}
               validateOnSubmit={true}
-              // validateOnBlur={true}
+              validateOnChange={false} // Evitará la validación al cambiar los campos
+              validateOnBlur={false} // Evitará la validación al desenfocar los campos
             >
               <Form className="mx-auto col-lg-9 col-sm-12 text-black fs-6 ">
                 <h1 className="text-center h1 mb-2 text-black">Contacto</h1>
@@ -63,8 +63,8 @@ const Contact = () => {
                     component={(props) => <Error message={props.children} />}
                   />
                 </div>
-                <div className="  d-flex justify-content-between mb-3 " >
-                  <div className="form-floating col-4" >
+                <div className="  d-flex justify-content-between mb-3 ">
+                  <div className="form-floating col-4">
                     <Field as="select" name="country" className="form-select ">
                       {countries.map((country) => (
                         <option key={country.value} value={country.value}>
