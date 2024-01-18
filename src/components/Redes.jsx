d1736754637dbe31148d210337dde02746e0b027
jonathan correa <jonathancorrea291292@hotmail.com>
2024-01-17 23:40:35 -0300

    probar el contact para que renderize

---
+++ src/components/Redes.jsx
@@ -0,0 +1,67 @@
+import { FaWhatsapp, FaFacebook, FaInstagram } from "react-icons/fa"; // Importng all icons for simplicity
+import { FiMail } from "react-icons/fi"; // Importing all icons for simplicity
+
+export const Redes = () => {
+  return (
+    <div className="lg-6 mt-5  container row col-lg-6 p- text-center text-black d-flex  align-items-center"
+    >
+      <div className="col-lg-12 text-black ">
+        <h3 className="h2">¿Necesitas Ayuda? </h3>
+        <ul className="list-unstyled mt-3">
+          <li className="  align-items-center mb-3">
+            <FaWhatsapp size={25} className="me-2 text-success  " />
+            <a
+              href="https://wa.me/5491140232792"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-black fw-bold"
+            >
+              (+54) 9 11 4023-2792
+            </a>
+          </li>
+          <li className="  align-items-center mb-3 ">
+            <FiMail size={25} className="me-2 " />
+            <a className="text-black fw-bold" href="mailto:secretaria@aepa.com.ar">
+              secretaria@aepa.com.ar
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="col-lg-12 mt-3 ">
+        <h3 className="h2 mb-3">Seguinos en las Redes</h3>
+        <ul className="list-unstyled">
+          <li className="  align-items-center mb-3">
+            <FaFacebook size={25} className="me-2  text-black " />
+            <a
+              href="https://www.facebook.com/profile.php?id=61551020911888"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-black  fw-bold fw-bold"
+            >
+              Facebook
+            </a>
+          </li>
+          <li className="  align-items-center mb-3">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="25"
+              height="25"
+              fill="currentColor"
+              class="bi bi-tiktok"
+              viewBox="0 0 16 16"
+              className="me-2 text-black"
+            >
+              <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z" />
+            </svg>{" "}
+            <a className="text-black fw-bold" href="https://www.tiktok.com/@aepa2023">Ticktok</a>
+          </li>
+          <li className="  align-items-center mb-3">
+            <FaInstagram size={25} className="me-2" />
+            <a className="text-black fw-bold" href="https://www.instagram.com/aepa318/">Instagram</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default Redes;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,48 +1,36 @@
-import { Formik, Field, Form, ErrorMessage } from "formik";
+ import { Formik, Field, Form, ErrorMessage  } from "formik";
 import { Error } from "../components/Error.jsx";
-import { FaWhatsapp, FaFacebook, FaInstagram } from "react-icons/fa"; // Importng all icons for simplicity
-import { FiMail } from "react-icons/fi"; // Importing all icons for simplicity
-import { validarEmail, validarNumeroTelefono } from "../security/Validacion.js";
-import "../css/contact.css"; 
+import { Alert } from "../components/Alert.jsx";
+import { Redes } from "../components/Redes.jsx";
+import "../css/contact.css"; // Asegúrate de tener un archivo CSS para estilos adicionales
+import * as Yup from 'yup';
+
 const countries = [
   { label: "AR (+54)", value: "+54" },
   { label: "USA (+1)", value: "+1" },
   { label: "UK (+44)", value: "+44" },
 ];
 
+const validationSchema = Yup.object().shape({
+  displayName: Yup.string().required("El nombre es obligatorio"),
+  telephone: Yup.string().required("El número es obligatorio"),
+  email: Yup.string().email("El correo no es válido").required("El correo es obligatorio"),
+  mensaje: Yup.string().required("El mensaje es obligatorio"),
+});
+
 const Contact = () => {
-  const validateForm = (values) => {
-    const errors = {};
-
-    if (!values.displayName) {
-      errors.displayName = "El nombre es obligatorio";
-    } else if (!values.telephone) {
-      errors.telephone = "El numero es obligatorio";
-    } else if (!values.email) {
-      errors.email = "El correo es obligatorio";
-    } else if (!values.mensaje) {
-      errors.mensaje = "El mensaje es obligatorio";
-    }
-    else if (!validarEmail(values.email)) {
-      errors.email = "El correo no es valido";
-    }
-    else if (!validarNumeroTelefono(values.telephone)) {
-      errors.telephone = "El telefono no es valido";
-    }
-    return errors;
-  };
-
-  const handleSubmit = (values, { setSubmitting, resetForm }) => {
-    // Tu lógica de envío del formulario aquí
+  const handleSubmit = (values, { setSubmitting, resetForm, setErrors }) => {
     console.log("Formulario enviado:", values);
+    setErrors({});
+    setSubmitting(true);
     resetForm();
-    setSubmitting(false);
   };
+
 
   return (
     <>
       <div
-        className=" background-contact  "
+        className="container"
       >
         <div className="row justify-content-center ">
           <div className="sign-in-container col-lg-6 ">
@@ -55,10 +43,10 @@ const Contact = () => {
                 mensaje: "",
                 country: countries[0].value,
               }}
-              validate={validateForm}
+              validationSchema={validationSchema}
               onSubmit={handleSubmit}
               validateOnSubmit={true}
-              validateOnBlur={true}
+              // validateOnBlur={true}
             >
               <Form className="mx-auto col-lg-9 col-sm-12 text-black fs-6 ">
                 <h1 className="text-center h1 mb-2 text-black">Contacto</h1>
@@ -75,8 +63,8 @@ const Contact = () => {
                     component={(props) => <Error message={props.children} />}
                   />
                 </div>
-                <div className="  d-flex justify-content-between mb-3 ">
-                  <div className="form-floating col-4  ">
+                <div className="  d-flex justify-content-between mb-3 " >
+                  <div className="form-floating col-4" >
                     <Field as="select" name="country" className="form-select ">
                       {countries.map((country) => (
                         <option key={country.value} value={country.value}>
@@ -136,65 +124,7 @@ const Contact = () => {
               </Form>
             </Formik>
           </div>
-          <div className="lg-6 mt-5  container row col-lg-6 p- text-center text-black d-flex  align-items-center"
-          >
-            <div className="col-lg-12 text-black ">
-              <h3 className="h2">¿Necesitas Ayuda? </h3>
-              <ul className="list-unstyled mt-3">
-                <li className="  align-items-center mb-3">
-                  <FaWhatsapp size={25} className="me-2 text-success  " />
-                  <a
-                    href="https://wa.me/5491140232792"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-black fw-bold"
-                  >
-                    (+54) 9 11 4023-2792
-                  </a>
-                </li>
-                <li className="  align-items-center mb-3 ">
-                  <FiMail size={25} className="me-2 " />
-                  <a className="text-black fw-bold" href="mailto:secretaria@aepa.com.ar">
-                    secretaria@aepa.com.ar
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div className="col-lg-12 mt-3 ">
-              <h3 className="h2 mb-3">Seguinos en las Redes</h3>
-              <ul className="list-unstyled">
-                <li className="  align-items-center mb-3">
-                  <FaFacebook size={25} className="me-2  text-black " />
-                  <a
-                    href="https://www.facebook.com/profile.php?id=61551020911888"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-black  fw-bold fw-bold"
-                  >
-                    Facebook
-                  </a>
-                </li>
-                <li className="  align-items-center mb-3">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="25"
-                    height="25"
-                    fill="currentColor"
-                    class="bi bi-tiktok"
-                    viewBox="0 0 16 16"
-                    className="me-2 text-black"
-                  >
-                    <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z" />
-                  </svg>{" "}
-                  <a className="text-black fw-bold" href="https://www.tiktok.com/@aepa2023">Ticktok</a>
-                </li>
-                <li className="  align-items-center mb-3">
-                  <FaInstagram size={25} className="me-2" />
-                  <a className="text-black fw-bold" href="https://www.instagram.com/aepa318/">Instagram</a>
-                </li>
-              </ul>
-            </div>
-          </div>
+          <Redes />
         </div>
       </div>
     </>
