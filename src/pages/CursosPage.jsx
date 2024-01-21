a5fe38d32f908c2e0dc54583ffe5fe6fbc9904ca
Paula Cottone <cottonepaula@gmail.com>
2024-01-21 14:06:09 -0300

    se modifico el curso page

--- src/pages/CursosPage.jsx
+++ src/pages/CursosPage.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import Cursos from "../components/Cursos";
 
 export default function CursosPage() {
-  return <h1 style={{ color: "black" }}>Cursos</h1>;
+  return <h1 style={{ color: "black" }}>Cursos Page</h1>;
 }
