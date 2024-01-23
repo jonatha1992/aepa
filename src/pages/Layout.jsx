097ee59a95d47dc139a86f7a1e8f8cf9aed1140d
ctrl1758 <franco.more1758@gmail.com>
2024-01-23 20:17:13 -0300

    rutas dinamicas

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -8,10 +8,12 @@ const Layout = ({ children }) => {
   const location = useLocation();
 
   // Decide si mostrar o no el componente Contact
-  const showContact = !["/Alumnos", "/admin", "/registro"].includes(location.pathname);
+  const showContact =
+    !["/Alumnos", "/admin", "/registro"].includes(location.pathname) &&
+    !location.pathname.startsWith("/unidades/");
 
   return (
-    <>+
+    <>
       <Header2 />
       <main style={{ marginTop: "220px", color: "white" }}>{children}</main>
       {showContact && <Contact />}
