f71c5d1f088b570700a5028a7da951a2469c9307
ctrl1758 <franco.more1758@gmail.com>
2024-02-06 22:48:04 -0300

    acomodo un poco los archivos,carpeta e importaciones

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -8,3 +8,5 @@ export { default as Login } from "./Login";
 export { default as Registro } from "./Registro";
 export { default as Dashbord } from "./Dashbord";
 export { default as AltaContenido } from "./AltaContenido";
+export { default as DashbordAlumnosLayout } from "./DashbordAlumnosLayout";
+export { default as Layout } from "./Layout";
