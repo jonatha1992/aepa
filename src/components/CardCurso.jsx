e94ad7e3ee41ed791c55d4e9aab6dcd1eaa82b34
jonathan correa <jonathancorrea291292@hotmail.com>
2024-02-09 19:27:17 -0300

    confirmar cambiios

---
+++ src/components/CardCurso.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "../css/curso.css";
+
+
+
+function Card({ title, image, description , autora,precio, duracion, inicio }) {
+    console.log(title, image, description);
+    return (
+        
+    );
+}
+
+export default Card;
