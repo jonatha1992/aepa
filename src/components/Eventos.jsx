15ccfc7428cd8aafa7b4651cfbe71252bbae8b83
Paula Cottone <cottonepaula@gmail.com>
2024-02-26 00:35:06 -0300

    Segunda Mod de Eventos con texto

--- src/components/Eventos.jsx
+++ src/components/Eventos.jsx
@@ -7,36 +7,32 @@ const Eventos = () => (
     <div className="eventos">
         <div className="encabezado">
             <h1>EVENTOS Y ANUNCIOS</h1>
-            <h2>Próximos inicios</h2>
+            <h2>Al unirse a nuestros eventos, tendrán acceso exclusivo a recursos educativos de alta calidad, contenido especializado y oportunidades de desarrollo profesional continuo, todo diseñado para impulsar su carrera hacia nuevos horizontes. Además, formar parte de la comunidad de la Asociación  de Enfermería Pediátrica Argentina les permitirá establecer conexiones duraderas y acceder a descuentos y beneficios especiales.</h2>
         </div>
 
         <div className="eventoCard">
             <div className="evento">
                 <div className="details">
-                    <div className="imagenInfo">
-                        <img src={imagen1} alt="imagen de evento" />
+                    <div className="imagenInfo" >
+                        <img src={imagen1} alt="imagen de evento" style={{ width: '100%', height: '180px'}} />
                     </div>
                     <div className="type">EVENTO</div>
                     <div className="name">III Encuentro de Neonatología</div>
                     <div className="date">22 de marzo</div>
                     <div className="time">10hs.</div>
-                    <div className="btnInfo">
-                        <button>MAS INFORMACIÓN</button>
-                    </div>
+                   
                 </div>
             </div>
             <div className="evento">
                 <div className="details">
-                    <div className="imagenInfo">
-                        <img src={imagen2} alt="imagen de anuncio" />
+                    <div className="imagenInfo" >
+                        <img className="imge"src={imagen2} alt="imagen de anuncio" style={{ width: '100%', height: '180px'}}/>
                     </div>
                     <div className="type">ANUNCIO</div>
                     <div className="name">III Encuentro Multidisciplinario de Enfermería</div>
                     <div className="date">22 de Noviembre</div>
                     <div className="time">Jornada Anual</div>
-                    <div className="btnInfo">
-                        <button>MAS INFORMACION</button>
-                    </div>
+                   
                 </div>
             </div>
         </div>
