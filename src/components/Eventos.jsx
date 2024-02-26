9b3fed91eb180069bb6309d18613878dd3c962de
jonathan correa <jonathancorrea291292@hotmail.com>
2024-02-25 21:22:03 -0300

    subida de imagenes de eventos

--- src/components/Eventos.jsx
+++ src/components/Eventos.jsx
@@ -1,49 +1,46 @@
 import React from "react";
 import "../css/eventos.css";
-
-
+import imagen1 from "../assets/neoevent.jpg";
+import imagen2 from "../assets/anuncio.jpg";
 
 const Eventos = () => (
+    <div className="eventos">
+        <div className="encabezado">
+            <h1>EVENTOS Y ANUNCIOS</h1>
+            <h2>Próximos inicios</h2>
+        </div>
 
-  <div className="eventos">
-     <div className="encabezado">
-        <h1>EVENTOS Y ANUNCIOS</h1>
-        <h2>Próximos inicios</h2>
-      </div>
-
-    <div className="eventoCard">
-     <div className="evento">
-     
-      <div className="details">
-        <div className="imagenInfo"><img src= "../assets/neoevent.jpg" alt="imagen de evento" /></div>
-        <div className="type">EVENTO</div>
-        <div className="name">III Encuentro de Neonatología</div>
-        <div className="date">22 de marzo</div>
-        <div className="time">10hs.</div>
-        <div className="btnInfo"><button>MAS INFORMACIÓN</button></div>
-      
-      
-      </div>
+        <div className="eventoCard">
+            <div className="evento">
+                <div className="details">
+                    <div className="imagenInfo">
+                        <img src={imagen1} alt="imagen de evento" />
+                    </div>
+                    <div className="type">EVENTO</div>
+                    <div className="name">III Encuentro de Neonatología</div>
+                    <div className="date">22 de marzo</div>
+                    <div className="time">10hs.</div>
+                    <div className="btnInfo">
+                        <button>MAS INFORMACIÓN</button>
+                    </div>
+                </div>
+            </div>
+            <div className="evento">
+                <div className="details">
+                    <div className="imagenInfo">
+                        <img src={imagen2} alt="imagen de anuncio" />
+                    </div>
+                    <div className="type">ANUNCIO</div>
+                    <div className="name">III Encuentro Multidisciplinario de Enfermería</div>
+                    <div className="date">22 de Noviembre</div>
+                    <div className="time">Jornada Anual</div>
+                    <div className="btnInfo">
+                        <button>MAS INFORMACION</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    <div className="evento">
-   
-      <div className="details">
-      <div className="imagenInfo"><img src="../assets/anuncio.jpg" alt="imagen de anuncio" /></div>
-        <div className="type">ANUNCIO</div>
-        <div className="name">III Encuentro Multidisciplinario de Enfermería</div>
-        <div className="date">22 de Noviembre</div>
-        <div className="time">Jornada Anual</div>
-        <div className="btnInfo"><button>MAS INFORMACION</button></div>
-      </div>
-    </div>
-    </div>
-    
-  </div>
-
-     
-
-
-
 );
 
 export default Eventos;
