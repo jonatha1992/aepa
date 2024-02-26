9e79665963399db3d5cb39b5bc71ac499f10c200
Paula Cottone <cottonepaula@gmail.com>
2024-02-25 21:06:01 -0300

    modificacion de evento

--- src/components/Eventos.jsx
+++ src/components/Eventos.jsx
@@ -1,34 +1,26 @@
 import React from "react";
 import "../css/eventos.css";
 
+
+
 const Eventos = () => (
 
-
-
   <div className="eventos">
-    
-      <div className="ecol-md-1 text-start">
+     <div className="encabezado">
         <h1>EVENTOS Y ANUNCIOS</h1>
         <h2>Próximos inicios</h2>
-         <div className="btn-opcion">
-          <button>mas info</button>
-          <button>mas info</button>
-          <button>mas info</button>
-          
-          </div>
-          
-          
       </div>
 
     <div className="eventoCard">
      <div className="evento">
      
       <div className="details">
+        <div className="imagenInfo"><img src= "../assets/neoevent.jpg" alt="imagen de evento" /></div>
         <div className="type">EVENTO</div>
         <div className="name">III Encuentro de Neonatología</div>
         <div className="date">22 de marzo</div>
         <div className="time">10hs.</div>
-        <div><button>MAS INFORMACION</button></div>
+        <div className="btnInfo"><button>MAS INFORMACIÓN</button></div>
       
       
       </div>
@@ -36,14 +28,22 @@ const Eventos = () => (
     <div className="evento">
    
       <div className="details">
+      <div className="imagenInfo"><img src="../assets/anuncio.jpg" alt="imagen de anuncio" /></div>
         <div className="type">ANUNCIO</div>
         <div className="name">III Encuentro Multidisciplinario de Enfermería</div>
         <div className="date">22 de Noviembre</div>
         <div className="time">Jornada Anual</div>
-        <div><button>MAS INFORMACION</button></div>
+        <div className="btnInfo"><button>MAS INFORMACION</button></div>
       </div>
     </div>
     </div>
+    
+  </div>
+
+     
+
+
+
 );
 
 export default Eventos;
