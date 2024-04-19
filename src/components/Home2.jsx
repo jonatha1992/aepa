f1a89f0cd4b4960d6517663925fe20721db5482c
ctrl1758 <franco.more1758@gmail.com>
2024-04-19 10:19:46 -0300

    se agrega integracion de pagos al cliente.

--- src/components/Home2.jsx
+++ src/components/Home2.jsx
@@ -1,19 +1,62 @@
 // src/Home2.jsx
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "../css/home2.css";
 import { Link } from "react-router-dom";
 import Nosotros from "../components/Nosotros";
+import { motion, useInView, useAnimation } from "framer-motion";
 
 import CursosSeccion from "./CursosSeccion";
 import Eventos from "../components/Eventos";
 
 function Home2() {
+  const refSection1 = useRef(null);
+  const refSection2 = useRef(null);
+  const refSection3 = useRef(null);
+
+  const isInViewSection1 = useInView(refSection1, { one: true });
+  const isInViewSection2 = useInView(refSection2, { one: true });
+  const isInViewSection3 = useInView(refSection3, { one: true });
+
+  const mainControls1 = useAnimation();
+  const mainControls2 = useAnimation();
+  const mainControls3 = useAnimation();
+
+  useEffect(() => {
+    if (isInViewSection1) {
+      mainControls1.start("visible");
+    }
+    console.log("isInViewSection1");
+  }, [isInViewSection1, mainControls1]);
+
+  useEffect(() => {
+    if (isInViewSection2) {
+      mainControls2.start("visible");
+    }
+    console.log("isInViewSection2");
+  }, [isInViewSection2, mainControls2]);
+
+  useEffect(() => {
+    if (isInViewSection3) {
+      mainControls3.start("visible");
+    }
+    console.log("isInViewSection3");
+  }, [isInViewSection3, mainControls3]);
+
   return (
     <>
       <section id="seccion1">
         <div className="container-fluid background-1">
           <div className="container mobile">
-            <div className="col-md-5 text-start">
+            <motion.div
+              className="col-md-5 text-start"
+              variants={{
+                hidden: { opacity: 0, y: 75 },
+                visible: { opacity: 1, u: 0 },
+              }}
+              initial="hidden"
+              animate="visible"
+              transition={{ duration: 0.5, delay: 0.25 }}
+            >
               <h2 className="titulo-hero">Bievenidos a AEPA</h2>
               <h2
                 style={{
@@ -36,21 +79,51 @@ function Home2() {
               >
                 Tu plataforma integral de formación para el personal de salud.
               </p>
-            </div>
+            </motion.div>
             <div className="col-md-3"></div>
           </div>
         </div>
       </section>
 
-      <section id="seccion2">
+      <motion.section
+        id="seccion2"
+        ref={refSection1}
+        variants={{
+          hidden: { opacity: 0, y: 10 },
+          visible: { opacity: 1, u: 0 },
+        }}
+        initial="hidden"
+        animate={mainControls1}
+        transition={{ duration: 1, delay: 0.5 }}
+      >
         <Nosotros />
-      </section>
-      <section id="seccion3">
+      </motion.section>
+      <motion.section
+        id="seccion3"
+        ref={refSection2}
+        variants={{
+          hidden: { opacity: 0, y: 10 },
+          visible: { opacity: 1, u: 0 },
+        }}
+        initial="hidden"
+        animate={mainControls2}
+        transition={{ duration: 1, delay: 0.5 }}
+      >
         <CursosSeccion />
-      </section>
-      <section id="seccion4">
+      </motion.section>
+      <motion.section
+        id="seccion4"
+        ref={refSection3}
+        variants={{
+          hidden: { opacity: 0, y: 10 },
+          visible: { opacity: 1, u: 0 },
+        }}
+        initial="hidden"
+        animate={mainControls3}
+        transition={{ duration: 1, delay: 0.5 }}
+      >
         <Eventos />
-      </section>
+      </motion.section>
     </>
   );
 }
--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -3,51 +3,72 @@ import presidenteImage from "../assets/lic-llanos-beatriz.png";
 import vicepresidenta from "../assets/lic-santini-maria.png";
 import secretario from "../assets/enf-sierra-jorge.png";
 import "../css/nosotros.css";
+import { motion } from "framer-motion";
 
 export default function Nosotros() {
-    return (
-        <div className="container-fluid background-6 p-5">
-            <div className=" d-flex justify-content-between  mt-5 flex-wrap align-items-center align-content-center">
-                <div className=" mx-auto  col-12  col-lg-6  justify-content-center align-items-center ">
-                    <h4 className="titulo-nosotros ">¿Quienes somos?</h4>
-                    <p className="parrafo-nosotros ">
-                        La Asociacion de Enfermeria Pediatrica Argentina, AEPA. Nació para tener una voz unificada que representara a los
-                        enfermeros pediátricos en Argentina. Somos un equipo comprometidos, con una premisa: brindar la mejor atención
-                        posible a nuestros pacientes y sus familias, buscamos unificar y fortalecer la profesión.
-                    </p>
-                    <p className="parrafo-nosotros">
-                        Los tres colores, representan a las instituciones emblemáticas en nuestro país en la atencion pediatrica: El color
-                        Celeste al
-                        <strong style={{ color: "var(--aepa-azul)" }}> Hospital Ricardo Gutiérrez</strong>, Naranja al{" "}
-                        <strong style={{ color: "var(--aepa-naranja)" }}> Hospital Pedro de Elizalde </strong> y Violeta al
-                        <strong style={{ color: "var(--aepa-violeta)" }}> Hospital Juan Garrahan</strong>.
-                    </p>
-                </div>
-                <div className=" row  col-12  col-lg-6 justify-content-center  ">
-                    <div className="imagen-organizacion col-12 col-sm-8 col-lg-8">
-                        <img src={presidenteImage} className=" w-50" alt="Presidente" />
-                        <div className="card-body">
-                            <h3 className="card-title">Presidente</h3>
-                            <p className="card-text text-center fw-bold">Lic. Llanos Beatriz</p>
-                        </div>
-                    </div>
-                    <div className="imagen-organizacion col-8 col-sm-6 col-lg-6 ">
-                        <img src={vicepresidenta} className=" w-50" alt="Vicepresidenta" />
-                        <div className="card-body">
-                            <h3 className="card-title">Vice Presidente</h3>
-                            <p className="card-text text-center fw-bold">Lic. Santini Maria</p>
-                        </div>
-                    </div>
-
-                    <div className=" imagen-organizacion  col-8 col-sm-6 col-lg-6">
-                        <img src={secretario} className=" w-50" alt="Secretario" />
-                        <div className="card-body">
-                            <h3 className="card-title">Secretario</h3>
-                            <p className="card-text text-center fw-bold ">Enf. Sierra Jorge</p>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="container-fluid background-6 p-5">
+      <div className=" d-flex justify-content-between  mt-5 flex-wrap align-items-center align-content-center">
+        <motion.div className=" mx-auto  col-12  col-lg-6  justify-content-center align-items-center ">
+          <h4 className="titulo-nosotros ">¿Quienes somos?</h4>
+          <p className="parrafo-nosotros ">
+            La Asociacion de Enfermeria Pediatrica Argentina, AEPA. Nació para
+            tener una voz unificada que representara a los enfermeros
+            pediátricos en Argentina. Somos un equipo comprometidos, con una
+            premisa: brindar la mejor atención posible a nuestros pacientes y
+            sus familias, buscamos unificar y fortalecer la profesión.
+          </p>
+          <p className="parrafo-nosotros">
+            Los tres colores, representan a las instituciones emblemáticas en
+            nuestro país en la atencion pediatrica: El color Celeste al
+            <strong style={{ color: "var(--aepa-azul)" }}>
+              {" "}
+              Hospital Ricardo Gutiérrez
+            </strong>
+            , Naranja al{" "}
+            <strong style={{ color: "var(--aepa-naranja)" }}>
+              {" "}
+              Hospital Pedro de Elizalde{" "}
+            </strong>{" "}
+            y Violeta al
+            <strong style={{ color: "var(--aepa-violeta)" }}>
+              {" "}
+              Hospital Juan Garrahan
+            </strong>
+            .
+          </p>
+        </motion.div>
+        <div className=" row  col-12  col-lg-6 justify-content-center  ">
+          <div className="imagen-organizacion col-12 col-sm-8 col-lg-8">
+            <img src={presidenteImage} className=" w-50" alt="Presidente" />
+            <div className="card-body">
+              <h3 className="card-title">Presidente</h3>
+              <p className="card-text text-center fw-bold">
+                Lic. Llanos Beatriz
+              </p>
             </div>
+          </div>
+          <div className="imagen-organizacion col-8 col-sm-6 col-lg-6 ">
+            <img src={vicepresidenta} className=" w-50" alt="Vicepresidenta" />
+            <div className="card-body">
+              <h3 className="card-title">Vice Presidente</h3>
+              <p className="card-text text-center fw-bold">
+                Lic. Santini Maria
+              </p>
+            </div>
+          </div>
+
+          <div className=" imagen-organizacion  col-8 col-sm-6 col-lg-6">
+            <img src={secretario} className=" w-50" alt="Secretario" />
+            <div className="card-body">
+              <h3 className="card-title">Secretario</h3>
+              <p className="card-text text-center fw-bold ">
+                Enf. Sierra Jorge
+              </p>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
