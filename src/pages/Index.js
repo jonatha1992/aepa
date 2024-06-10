83fb67e25ec7471b11bb7a51fe439740c5240497
jonathan correa <jonathancorrea291292@hotmail.com>
2024-06-10 05:31:07 -0300

    cambios para probar la subida

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,10 +1,10 @@
 // src/pages/index.js
 /* export { default as Nosotros } from "../components/Nosotros"; */
+/* export { default as Dashbord } from "./Dashbord"; */
 export { default as Talleres } from "./Talleres";
 export { default as Socios } from "./Socios";
 export { default as Login } from "./Login";
 export { default as Registro } from "./Registro";
-/* export { default as Dashbord } from "./Dashbord"; */
 export { default as AltaContenido } from "./AltaContenido";
 export { default as DashbordAlumnosLayout } from "./DashbordAlumnosLayout";
 export { default as Layout } from "./Layout";
