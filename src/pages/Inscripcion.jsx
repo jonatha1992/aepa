2f35ec47127d3e943a897ac4e6cf2b9688d07b65
jonatha1992 <jonathancorrea291292@hotmail.com>
2024-06-21 05:37:47 -0300

    cambios del cliente en la vista del card y la preinscripcion que se muestren mas datos y borrado del precio

--- src/pages/Inscripcion.jsx
+++ src/pages/Inscripcion.jsx
@@ -14,273 +14,277 @@ import { Button } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 
 export default function Inscripcion() {
-  const { User } = useAuth();
-  const { cursoid } = useParams();
-  const [curso, setCurso] = useState(null);
-  const [preferenceId, setPreferenceId] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [cursoRepetido, setCursoRepetido] = useState(false);
-  const { cursos } = useContext(AlumnosContext);
-  const navigate = useNavigate(); // Cambiado a useNavigate
+    const { User } = useAuth();
+    const { cursoid } = useParams();
+    const [curso, setCurso] = useState(null);
+    const [preferenceId, setPreferenceId] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [cursoRepetido, setCursoRepetido] = useState(false);
+    const { cursos } = useContext(AlumnosContext);
+    const navigate = useNavigate(); // Cambiado a useNavigate
 
-  initMercadoPago("APP_USR-d1e6b7d2-9956-4c52-ba39-46df48157a58", {
-    locale: "es-AR",
-  });
+    initMercadoPago("APP_USR-d1e6b7d2-9956-4c52-ba39-46df48157a58", {
+        locale: "es-AR",
+    });
 
-  const createPreference = async () => {
-    try {
-      if (!User || !User.uid) {
-        console.log(
-          "El usuario no está autenticado. No se creará la preferencia."
-        );
-        navigate(`/login?from=/inscripcion/${cursoid}`);
-        return;
-      }
-      setLoading(true);
-      const response = await axios.post(
-        "https://us-central1-aepa-86ed6.cloudfunctions.net/app/create_preference",
-        {
-          description: curso.title,
-          title: "AEPA",
-          price: curso.price,
-          quantity: 1,
-          cursoid: cursoid,
-          uid: User.uid,
+    const createPreference = async () => {
+        try {
+            if (!User || !User.uid) {
+                console.log("El usuario no está autenticado. No se creará la preferencia.");
+                navigate(`/login?from=/inscripcion/${cursoid}`);
+                return;
+            }
+            setLoading(true);
+            const response = await axios.post(
+                "https://us-central1-aepa-86ed6.cloudfunctions.net/app/create_preference",
+                {
+                    description: curso.title,
+                    title: "AESFRON",
+                    price: curso.price,
+                    quantity: 1,
+                    cursoid: cursoid,
+                    uid: User.uid,
+                }
+            );
+
+            const { id } = response.data;
+            return id;
+        } catch (error) {
+            console.log("bandera", error);
+        } finally {
+            setLoading(false);
         }
-      );
-
-      const { id } = response.data;
-      return id;
-    } catch (error) {
-      console.log("bandera", error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const handleBuy = async () => {
-    const existe = cursos.some((item) => item.cursoid === cursoid);
-
-    if (existe) {
-      setCursoRepetido(true);
-    } else {
-      const id = await createPreference();
-      if (id) {
-        setPreferenceId(id);
-      }
-    }
-  };
-
-  useEffect(() => {
-    const fetchCurso = async () => {
-      try {
-        const cursoData = await getCurso(cursoid);
-        setCurso(cursoData);
-      } catch (error) {
-        console.error("Error al obtener detalles del curso", error);
-      }
     };
 
-    /* const checkAndSetEstado = () => {
+    const handleBuy = async () => {
+        const existe = cursos.some((item) => item.cursoid === cursoid);
+
+        if (existe) {
+            setCursoRepetido(true);
+        } else {
+            const id = await createPreference();
+            if (id) {
+                setPreferenceId(id);
+            }
+        }
+    };
+
+    useEffect(() => {
+        const fetchCurso = async () => {
+            try {
+                const cursoData = await getCurso(cursoid);
+                setCurso(cursoData);
+            } catch (error) {
+                console.error("Error al obtener detalles del curso", error);
+            }
+        };
+
+        /* const checkAndSetEstado = () => {
       const existe = cursos.some((item) => item.cursoid == cursoid);
       setCursoRepetido(existe);
     }; */
 
-    if (cursoid) {
-      fetchCurso();
-      /* checkAndSetEstado(); */
+        if (cursoid) {
+            fetchCurso();
+            /* checkAndSetEstado(); */
+        }
+    }, [cursoid]);
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [cursoid]);
+
+    if (!curso) {
+        return (
+            <Box sx={{ width: 300 }}>
+                <Skeleton />
+                <Skeleton animation="wave" />
+                <Skeleton animation={false} />
+            </Box>
+        );
     }
-  }, [cursoid]);
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [cursoid]);
+    const customization = {
+        visual: {
+            buttonBackground: "black",
+            buttonHeight: "68px",
+        },
+    };
 
-  if (!curso) {
     return (
-      <Box sx={{ width: 300 }}>
-        <Skeleton />
-        <Skeleton animation="wave" />
-        <Skeleton animation={false} />
-      </Box>
-    );
-  }
-
-  const customization = {
-    visual: {
-      buttonBackground: "black",
-      buttonHeight: "68px",
-    },
-  };
-
-  return (
-    <div className="fondo-panta" style={{}}>
-      <div className="header-inscripcion">
-        <div className="d-flex flex-column">
-          <div className="info-inscripcion" style={{}}>
-            <h2 className="fs-1 fw-bold">{curso.title}</h2>
-            <h3>${curso.price.toLocaleString("es-AR")} ARS</h3>
-          </div>
-          <Button
-            className="d-inline-block align-baseline  fs-4 fw-bold  hover-primary mt-4 bg-gradient "
-            type="submit"
-            variant="contained"
-            fullWidth
-            disabled={loading}
-            /* onClick={handleBuy} */
-          >
-            PROXIMAMENTE.🔽
-          </Button>
-        </div>
-        <div className="contenedor-imagen-curso" style={{ padding: "1rem" }}>
-          <img src={curso.imageUrl} alt="" />
-        </div>
-      </div>
-
-      <Modal
-        open={loading}
-        BackdropComponent={Backdrop}
-        onClose={() => setLoading(false)}
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <CircularProgress color="primary" />
-      </Modal>
-
-      {preferenceId && !cursoRepetido && (
-        <Modal
-          open={!loading}
-          onClose={() => setPreferenceId(null)}
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <div
-            style={{
-              maxWidth: "400px",
-              padding: "20px",
-              background: "#fff",
-              borderRadius: "8px",
-            }}
-          >
-            {/* Resumen de la operación */}
-            <h3>Confirmacion</h3>
-            <p>Curso: {curso.title}</p>
-            <p>Precio: {curso.price}</p>
-
-            {/* Componente Wallet */}
-            <div style={{ marginTop: "20px" }}>
-              <Wallet
-                initialization={{ preferenceId }}
-                customization={customization}
-              />
+        <div className="fondo-panta" style={{}}>
+            <div className="header-inscripcion">
+                <div className="d-flex flex-column">
+                    <div className="info-inscripcion" style={{}}>
+                        <h2 className="fs-1 fw-bold">{curso.title}</h2>
+                        {/* <h3>${curso.price.toLocaleString("es-AR")} ARS</h3> */}
+                    </div>
+                    <Button
+                        className="d-inline-block align-baseline  fs-4 fw-bold  hover-primary mt-4 bg-gradient "
+                        type="submit"
+                        variant="contained"
+                        fullWidth
+                        disabled={loading}
+                        /* onClick={handleBuy} */
+                    >
+                        PROXIMAMENTE.🔽
+                    </Button>
+                </div>
+                <div className="contenedor-imagen-curso" style={{ padding: "1rem" }}>
+                    <img src={curso.imageUrl} alt="" />
+                </div>
             </div>
-          </div>
-        </Modal>
-      )}
 
-      {cursoRepetido && (
-        <Modal
-          open={!loading}
-          onClose={() => setCursoRepetido(false)}
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <div
-            style={{
-              maxWidth: "400px",
-              padding: "20px",
-              background: "#fff",
-              borderRadius: "8px",
-            }}
-          >
-            <h3>¡Ya estás inscrito!</h3>
-            <p>Ya te encuentras inscrito en este curso.</p>
-            <button
-              onClick={() => {
-                navigate("/alumnos");
-                setCursoRepetido(false);
-              }}
+            <Modal
+                open={loading}
+                BackdropComponent={Backdrop}
+                onClose={() => setLoading(false)}
+                style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}
             >
-              Ir a Alumnos
-            </button>
-          </div>
-        </Modal>
-      )}
+                <CircularProgress color="primary" />
+            </Modal>
 
-      <div className="body-inscripcion d-flex  flex-wrap  justify-content-between ">
-        <div className=" col-12">
-          <h2>Misión</h2>
-          <p className="fs-6 ">{curso.description}</p>
-        </div>
-        <div className=" col-12  ">
-          <h2>Objetivos</h2>
+            {preferenceId && !cursoRepetido && (
+                <Modal
+                    open={!loading}
+                    onClose={() => setPreferenceId(null)}
+                    style={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                    }}
+                >
+                    <div
+                        style={{
+                            maxWidth: "400px",
+                            padding: "20px",
+                            background: "#fff",
+                            borderRadius: "8px",
+                        }}
+                    >
+                        {/* Resumen de la operación */}
+                        <h3>Confirmacion</h3>
+                        <p>Curso: {curso.title}</p>
+                        <p>Precio: {curso.price}</p>
 
-          <ul className=" fs-6">
-            {curso.objetivos.map((objetivo, index) => (
-              <li key={index}>{objetivo}</li>
-            ))}
-          </ul>
-        </div>
-        <div className=" col-12 col-md-6">
-          <h2>Contenido</h2>
-          <AcordeonUnidades cursoid={cursoid} />
-        </div>
-        <div className="col-12 col-md-5  ">
-          <h2>Detalles</h2>
-          <div className="row">
-            <div className="col-6 ">
-              <h3>
-                Clases<p>{curso.classes}</p>
-              </h3>
-            </div>
-            <div className="col-6">
-              <h3>
-                Duración<p>{curso.duration}</p>
-              </h3>
-            </div>
-            <div className="col-6">
-              <h3>
-                Inicio<p>{curso.start}</p>
-              </h3>
-            </div>
-            <div className="col-6">
-              <h3>
-                Carga Horaria<p>{curso.workload + " Horas"}</p>
-              </h3>
-            </div>
+                        {/* Componente Wallet */}
+                        <div style={{ marginTop: "20px" }}>
+                            <Wallet initialization={{ preferenceId }} customization={customization} />
+                        </div>
+                    </div>
+                </Modal>
+            )}
 
-            <div className="col-6">
-              <h3>
-                Autor<p>{curso.author}</p>
-              </h3>
+            {cursoRepetido && (
+                <Modal
+                    open={!loading}
+                    onClose={() => setCursoRepetido(false)}
+                    style={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                    }}
+                >
+                    <div
+                        style={{
+                            maxWidth: "400px",
+                            padding: "20px",
+                            background: "#fff",
+                            borderRadius: "8px",
+                        }}
+                    >
+                        <h3>¡Ya estás inscrito!</h3>
+                        <p>Ya te encuentras inscrito en este curso.</p>
+                        <button
+                            onClick={() => {
+                                navigate("/alumnos");
+                                setCursoRepetido(false);
+                            }}
+                        >
+                            Ir a Alumnos
+                        </button>
+                    </div>
+                </Modal>
+            )}
+
+            <div className="body-inscripcion d-flex  flex-wrap  justify-content-between ">
+                <div className=" col-12">
+                    <h2>Objetivo General</h2>
+                    <p className="fs-6 ">{curso.description}</p>
+                </div>
+                <div className=" col-12  ">
+                    <h2>Objetivos Particular</h2>
+
+                    <ul className=" fs-6">
+                        {curso.objetivos.map((objetivo, index) => (
+                            <li key={index}>{objetivo}</li>
+                        ))}
+                    </ul>
+                </div>
+                <div className=" col-12 col-md-6">
+                    <h2>Contenido</h2>
+                    <AcordeonUnidades cursoid={cursoid} />
+                </div>
+                <div className="col-12 col-md-5  ">
+                    <h2>Detalles</h2>
+                    <div className="row">
+                        <div className="col-6">
+                            <h3>
+                                Coordinación<p> {curso.coordinacion}</p>
+                            </h3>
+                        </div>
+                        <div className="col-6 ">
+                            <h3>
+                                Docentes
+                                <ul className=" fs-6">
+                                    {curso.disertantes.map((disertante, index) => (
+                                        <li key={index}> {disertante}</li>
+                                    ))}
+                                </ul>
+                            </h3>
+                        </div>
+                        <div className="col-6">
+                            <h3>
+                                Carga Horaria<p>{curso.workload + " Horas"}</p>
+                            </h3>
+                        </div>
+                        <div className="col-6">
+                            <h3>
+                                Duración<p>{curso.duration}</p>
+                            </h3>
+                        </div>
+                        <div className="col-6">
+                            <h3>
+                                Inicio<p>{curso.start}</p>
+                            </h3>
+                        </div>
+                        <div className="col-6 ">
+                            <h3>
+                                Clases<p>{curso.classes}</p>
+                            </h3>
+                        </div>
+                        <div className="col-6 ">
+                            <h3>
+                                Modalidad<p>{curso.modalidad}</p>
+                            </h3>
+                        </div>
+                        <div className="col-6 ">
+                            <h3>
+                                Mail de contacto<p>{curso.mail}</p>
+                            </h3>
+                        </div>
+                        <div className="col-6 ">
+                            <h3>
+                                Evaluacion<p>{curso.test}</p>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="col-6">
-              <h3>
-                Coordinación<p> {curso.coordinacion}</p>
-              </h3>
-            </div>
-            <div className="col-12 ">
-              <h3>
-                Disertantes
-                <ul className=" fs-6">
-                  {curso.disertantes.map((disertante, index) => (
-                    <li key={index}> {disertante}</li>
-                  ))}
-                </ul>
-              </h3>
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
--- src/pages/Socios.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Socios() {
-  return (
-    <div>
-      <h1 style={{ color: "black" }}>Socios</h1>
-    </div>
-  );
-}
