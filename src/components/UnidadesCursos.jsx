303f6a7c1286c4fa05b2a66500b98846b7a047e3
jonatha1992 <jonathancorrea291292@hotmail.com>
2024-06-22 12:01:07 -0300

    cambios en unidades cursos

--- src/components/UnidadesCursos.jsx
+++ src/components/UnidadesCursos.jsx
@@ -6,161 +6,131 @@ import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Typography from "@mui/material/Typography";
 import PictureAsPdfIcon from "@mui/icons-material/PictureAsPdf";
 import LinkIcon from "@mui/icons-material/Link";
-import Breadcrumbs from "./Breadcrumbs";
-import { ContenidoXCurso } from "../firebase.js";
 import DownloadForOfflineIcon from "@mui/icons-material/DownloadForOffline";
+import { getModulos } from "../controllers/controllerCurso";
 
 const UnidadesCursos = ({ activeCourse }) => {
-  const { id, title } = activeCourse || {};
-  const [contenido, setContenido] = useState([]);
-  const [openUnidad, setOpenUnidad] = useState(true);
+    const { id, title, mail } = activeCourse || {};
+    console.log("activeCourse", activeCourse);
+    const [contenido, setContenido] = useState([]);
 
-  useEffect(() => {
-    const fetchDataFromFirebase = async () => {
-      try {
-        const contenidoData = await ContenidoXCurso(id);
-        setContenido(contenidoData);
-      } catch (error) {
-        console.error("Error al obtener datos de Firebase:", error);
-      }
+    useEffect(() => {
+        const fetchDataFromFirebase = async () => {
+            try {
+                const contenidoData = await getModulos(id);
+                setContenido(contenidoData);
+            } catch (error) {
+                console.error("Error al obtener datos de Firebase:", error);
+            }
+        };
+
+        if (id) {
+            fetchDataFromFirebase();
+        }
+    }, [id]);
+
+    const handleMaterialClick = (datourl) => {
+        window.open(datourl, "_blank");
     };
 
-    fetchDataFromFirebase();
-  }, [id]);
-
-  const obtenerTiposUnicos = () => {
-    const tiposSet = new Set();
-
-    contenido.forEach((unidad) => {
-      tiposSet.add(unidad.tipo);
-    });
-
-    return Array.from(tiposSet);
-  };
-
-  const tiposUnicos = obtenerTiposUnicos();
-
-  const arbolUnidadesTipos = contenido.reduce((arbol, unidad) => {
-    if (!arbol[unidad.unidad]) {
-      arbol[unidad.unidad] = {};
-    }
-
-    if (!arbol[unidad.unidad][unidad.tipo]) {
-      arbol[unidad.unidad][unidad.tipo] = [];
-    }
-
-    arbol[unidad.unidad][unidad.tipo].push(unidad);
-
-    return arbol;
-  }, {});
-
-  const handleToggleUnidad = (numero) => {
-    setOpenUnidad((prevOpenUnidad) =>
-      prevOpenUnidad === numero ? null : numero
-    );
-  };
-
-  const handleMaterialClick = (datourl) => {
-    window.open(datourl, "_blank");
-  };
-
-  return (
-    <div className="container" style={{}}>
-      <div
-        className=""
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "flex-start",
-        }}
-      >
-        <Typography
-          variant="h4"
-          style={{
-            background: "white",
-            color: "#606468",
-            textAlign: "start",
-            textTransform: "uppercase",
-            padding: "10px",
-            borderRadius: "5px",
-          }}
-        >
-          {title}
-        </Typography>
-        <div style={{ color: "black", background: "#5d809d" }}>
-          {Object.entries(arbolUnidadesTipos).map(([numero, tipos]) => (
-            <Accordion key={numero} defaultExpanded>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
+    return (
+        <div className="container ">
+            <div
+                className=""
                 style={{
-                  background: "var(--color6)",
-                  color: "white",
-                  fontSize: "1.5rem",
-                  marginBottom: "5px",
-                  borderRadius: "5px",
-                  padding: "5px",
-                  cursor: "pointer",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "flex-start",
                 }}
-              >
-                Unidad {numero}
-              </AccordionSummary>
-              <AccordionDetails
-                style={{
-                  backgroundColor: "rgb(182 199 213)",
-                  paddingBottom: "0.5rem",
-                  borderRadius: "5px",
-                }}
-              >
-                {Object.entries(tipos).map(([tipo, detalles]) => (
-                  <div key={tipo} style={{ marginBottom: "10px" }}>
-                    <div className="d-flex align-center">
-                      {tipo === "pdf" && (
-                        <PictureAsPdfIcon
-                          style={{
-                            width: "1.5em",
-                            height: "1.5em",
-                            marginRight: "5px",
-                          }}
-                        />
-                      )}
-                      {tipo === "link" && (
-                        <LinkIcon
-                          style={{
-                            width: "1.5em",
-                            height: "1.5em",
-                            marginRight: "5px",
-                          }}
-                        />
-                      )}
-                      <Typography variant="h6" className="encabezado-tipo">
-                        {tipo}
-                      </Typography>
-                    </div>
-                    {detalles.map((detalle) => (
-                      <div
-                        key={detalle.id}
-                        className="titulo-contenido"
-                        onClick={() => handleMaterialClick(detalle.datourl)}
-                        style={{ cursor: "pointer" }}
-                      >
-                        <div
-                          className="d-flex flex-row align-items-center"
-                          style={{ width: "75%" }}
-                        >
-                          <DownloadForOfflineIcon />
-                          <Typography>{detalle.titulo}</Typography>
-                        </div>
-                      </div>
+            >
+                <div className="d-flex bg-white justify-content-center flex-wrap ">
+                    <Typography
+                        variant="h6"
+                        className=" text-capitalize p-2 mb-3 rounded col-lg-6 col-12   "
+                        style={{ color: "#606468" }}
+                    >
+                        <span className="fw-bold text-uppercase">Curso: </span> {title}
+                    </Typography>
+                    <Typography
+                        variant="h6"
+                        className=" text-lowercase p-2 mb-3 rounded col-lg-5 col-12"
+                        style={{ color: "#606468" }}
+                    >
+                        <span className="fw-bold text-uppercase">Mail: </span> {mail}
+                    </Typography>
+                </div>
+
+                <div style={{ color: "black", background: "#5d809d" }}>
+                    {contenido.map((unidad) => (
+                        <Accordion key={unidad.id} defaultExpanded>
+                            <AccordionSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                style={{
+                                    background: "var(--color6)",
+                                    color: "white",
+                                    fontSize: "1.5rem",
+                                    marginBottom: "5px",
+                                    borderRadius: "5px",
+                                    padding: "5px",
+                                    cursor: "pointer",
+                                }}
+                            >
+                                {unidad.titulo}
+                            </AccordionSummary>
+                            <AccordionDetails
+                                style={{
+                                    backgroundColor: "rgb(182 199 213)",
+                                    paddingBottom: "0.5rem",
+                                    borderRadius: "5px",
+                                }}
+                            >
+                                {unidad.items.map((item) => (
+                                    <div
+                                        key={item.id}
+                                        className="titulo-contenido "
+                                        onClick={() => handleMaterialClick(item.url)}
+                                        style={{ cursor: "pointer" }}
+                                    >
+                                        <div className="d-flex flex-row align-items-center" style={{ width: "100%" }}>
+                                            {item.tipo === "pdf" && (
+                                                <PictureAsPdfIcon
+                                                    style={{
+                                                        width: "1.5em",
+                                                        height: "1.5em",
+                                                        marginRight: "5px",
+                                                    }}
+                                                />
+                                            )}
+                                            <Typography>{item.titulo}</Typography>
+                                            {item.tipo === "link" && (
+                                                <LinkIcon
+                                                    style={{
+                                                        width: "1.5em",
+                                                        height: "1.5em",
+                                                        marginRight: "5px",
+                                                    }}
+                                                />
+                                            )}
+                                            {item.tipo === "Info" && (
+                                                <LinkIcon
+                                                    style={{
+                                                        width: "1.5em",
+                                                        height: "1.5em",
+                                                        marginRight: "5px",
+                                                    }}
+                                                />
+                                            )}
+                                            <DownloadForOfflineIcon />
+                                        </div>
+                                    </div>
+                                ))}
+                            </AccordionDetails>
+                        </Accordion>
                     ))}
-                  </div>
-                ))}
-              </AccordionDetails>
-            </Accordion>
-          ))}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default UnidadesCursos;
--- src/firebase.js
+++ src/firebase.js
@@ -1,36 +1,36 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import {
-  getFirestore,
-  collection,
-  getDocs,
-  doc,
-  getDoc,
-  setDoc,
-  updateDoc,
-  addDoc,
-  deleteDoc,
-  query,
-  where,
+    getFirestore,
+    collection,
+    getDocs,
+    doc,
+    getDoc,
+    setDoc,
+    updateDoc,
+    addDoc,
+    deleteDoc,
+    query,
+    where,
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js";
 
 import {
-  getStorage,
-  uploadBytes,
-  ref,
-  getDownloadURL,
-  deleteObject,
+    getStorage,
+    uploadBytes,
+    ref,
+    getDownloadURL,
+    deleteObject,
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-storage.js";
 
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCcjRgJQwqJewSs85WRCkSyMRnSF6jMlHk",
-  authDomain: "aesfron-69a52.firebaseapp.com",
-  projectId: "aesfron-69a52",
-  storageBucket: "aesfron-69a52.appspot.com",
-  messagingSenderId: "429701236102",
-  appId: "1:429701236102:web:e6062b3df1a3398003d807",
-  measurementId: "G-H7ZGVD4P5V",
+    apiKey: "AIzaSyCcjRgJQwqJewSs85WRCkSyMRnSF6jMlHk",
+    authDomain: "aesfron-69a52.firebaseapp.com",
+    projectId: "aesfron-69a52",
+    storageBucket: "aesfron-69a52.appspot.com",
+    messagingSenderId: "429701236102",
+    appId: "1:429701236102:web:e6062b3df1a3398003d807",
+    measurementId: "G-H7ZGVD4P5V",
 };
 
 // Initialize Firebase
@@ -41,167 +41,137 @@ const storage = getStorage(app);
 export const auth = getAuth(app);
 
 export async function agregarCurso(dati) {
-  const newCourseRef = await addDoc(collection(db, "cursos"), dati);
-  const cursoID = newCourseRef.id;
-  return cursoID;
+    const newCourseRef = await addDoc(collection(db, "cursos"), dati);
+    const cursoID = newCourseRef.id;
+    return cursoID;
 }
 
 export async function uploadFiles(file) {
-  const storageRef = ref(storage, crypto.randomUUID());
-  const metadata = {
-    contentType: "image/jpeg",
-  };
-  const meta = await uploadBytes(storageRef, file, metadata);
-  console.log(meta);
-  const url = await getDownloadURL(storageRef);
-  return url;
+    const storageRef = ref(storage, crypto.randomUUID());
+    const metadata = {
+        contentType: "image/jpeg",
+    };
+    const meta = await uploadBytes(storageRef, file, metadata);
+    console.log(meta);
+    const url = await getDownloadURL(storageRef);
+    return url;
 }
 
 export async function deletear(id, tabla) {
-  const docRef = doc(db, `${tabla}`, id);
-  const docSnap = await getDoc(docRef);
+    const docRef = doc(db, `${tabla}`, id);
+    const docSnap = await getDoc(docRef);
 
-  if (docSnap.exists()) {
-    console.log("Document data:", docSnap.data());
-    let dato = docSnap.data();
-    console.log("intentando eliminar la imagen:", dato.imagen);
+    if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+        let dato = docSnap.data();
+        console.log("intentando eliminar la imagen:", dato.imagen);
 
-    try {
-      if (tabla == "cursos") {
-        await deleteFile(dato.image);
-        await deleteDoc(doc(db, `${tabla}`, id));
-        return true;
-        /* notyf.error("Pic eliminado de la galeria"); */
-        /* administrarGaleria(); */
-      }
-    } catch (error) {
-      console.log("algo paso", error);
-      return false;
-      /* notyf.error("ocurrio un error en la operacion"); */
+        try {
+            if (tabla == "cursos") {
+                await deleteFile(dato.image);
+                await deleteDoc(doc(db, `${tabla}`, id));
+                return true;
+                /* notyf.error("Pic eliminado de la galeria"); */
+                /* administrarGaleria(); */
+            }
+        } catch (error) {
+            console.log("algo paso", error);
+            return false;
+            /* notyf.error("ocurrio un error en la operacion"); */
+        }
     }
-  }
 }
 
 async function deleteFile(url) {
-  console.log("la url desde bd: ", url);
-  // var fileRef = storage.refFromURL(url);
-  var fileRef = ref(storage, url);
+    console.log("la url desde bd: ", url);
+    // var fileRef = storage.refFromURL(url);
+    var fileRef = ref(storage, url);
 
-  try {
-    await deleteObject(fileRef);
-    console.log("Archivo eliminado exitosamente.");
-  } catch (error) {
-    console.log("Error al eliminar archivo:", error);
-  }
+    try {
+        await deleteObject(fileRef);
+        console.log("Archivo eliminado exitosamente.");
+    } catch (error) {
+        console.log("Error al eliminar archivo:", error);
+    }
 }
 
 const CursosInscriptos = async (uid) => {
-  try {
-    // Realiza la consulta a Firebase
-    const miscursosRef = await collection(db, "inscripciones");
-    const q = await query(miscursosRef, where("uid", "==", uid));
-    const querySnapshot = await getDocs(q);
+    try {
+        // Realiza la consulta a Firebase
+        const miscursosRef = await collection(db, "inscripciones");
+        const q = await query(miscursosRef, where("uid", "==", uid));
+        const querySnapshot = await getDocs(q);
 
-    // Extrae los datos de la consulta y actualiza el estado
-    const cursosArray = [];
-    querySnapshot.forEach((doc) => {
-      cursosArray.push({ inscripcionid: doc.id, ...doc.data() });
-    });
-    // Realiza una segunda consulta para obtener los detalles de cada curso
-    const cursosDetallesArray = await Promise.all(
-      cursosArray.map(async (curso) => {
-        const cursoDetallesRef = doc(db, "cursos", curso.cursoid);
-        const cursoDetallesSnapshot = await getDoc(cursoDetallesRef);
-        return { ...curso, detalles: cursoDetallesSnapshot.data() };
-      })
-    );
+        // Extrae los datos de la consulta y actualiza el estado
+        const cursosArray = [];
+        querySnapshot.forEach((doc) => {
+            cursosArray.push({ inscripcionid: doc.id, ...doc.data() });
+        });
+        // Realiza una segunda consulta para obtener los detalles de cada curso
+        const cursosDetallesArray = await Promise.all(
+            cursosArray.map(async (curso) => {
+                const cursoDetallesRef = doc(db, "cursos", curso.cursoid);
+                const cursoDetallesSnapshot = await getDoc(cursoDetallesRef);
+                return { ...curso, detalles: cursoDetallesSnapshot.data() };
+            })
+        );
 
-    return cursosDetallesArray;
-  } catch (error) {
-    console.error("Error en la operación asincrónica:", error);
-  }
+        return cursosDetallesArray;
+    } catch (error) {
+        console.error("Error en la operación asincrónica:", error);
+    }
 };
 
 import { agregarItemsModulo } from "./security/Tools";
 const itemsToBeAdded = [
-  {
-    tipo: "video",
-    titulo: "Cuidados para el neurodesarrollo. ",
-    url: "http://link-al-video.com",
-  },
-  {
-    tipo: "pdf",
-    titulo: "La importancia de la leche humana en UCIN",
-    url: "http://link-al-pdf.com",
-  },
-  {
-    tipo: "video",
-    titulo: "Drogas de reanimación",
-    url: "http://link-al-video.com",
-  },
-  {
-    tipo: "video",
-    titulo: "Cuidado Canguro ",
-    url: "http://link-al-video.com",
-  },
-  {
-    tipo: "pdf",
-    titulo: "Niveles de ruido en UCIN- Prevención de la hipoacusia",
-    url: "http://link-al-pdf.com",
-  },
-  {
-    tipo: "video",
-    titulo: "Paciente Recién Nacido quirúrgico",
-    url: "http://link-al-video.com",
-  },
-  {
-    tipo: "video",
-    titulo: "Cuidado infectologico en UCIN",
-    url: "http://link-al-video.com",
-  },
-  {
-    tipo: "pdf",
-    titulo: "Trastornos de la deglución en el Recién Nacido de Riesgo",
-    url: "http://link-al-pdf.com",
-  },
-  {
-    tipo: "video",
-    titulo:
-      "Detección del síndrome genético en el Recién Nacido/ Cardiopatías neonatales",
-    url: "http://link-al-video.com",
-  },
+    {
+        tipo: "video",
+        titulo: "Cuidados para el neurodesarrollo. ",
+        url: "http://link-al-video.com",
+    },
+    {
+        tipo: "pdf",
+        titulo: "La importancia de la leche humana en UCIN",
+        url: "http://link-al-pdf.com",
+    },
+    {
+        tipo: "video",
+        titulo: "Drogas de reanimación",
+        url: "http://link-al-video.com",
+    },
+    {
+        tipo: "video",
+        titulo: "Cuidado Canguro ",
+        url: "http://link-al-video.com",
+    },
+    {
+        tipo: "pdf",
+        titulo: "Niveles de ruido en UCIN- Prevención de la hipoacusia",
+        url: "http://link-al-pdf.com",
+    },
+    {
+        tipo: "video",
+        titulo: "Paciente Recién Nacido quirúrgico",
+        url: "http://link-al-video.com",
+    },
+    {
+        tipo: "video",
+        titulo: "Cuidado infectologico en UCIN",
+        url: "http://link-al-video.com",
+    },
+    {
+        tipo: "pdf",
+        titulo: "Trastornos de la deglución en el Recién Nacido de Riesgo",
+        url: "http://link-al-pdf.com",
+    },
+    {
+        tipo: "video",
+        titulo: "Detección del síndrome genético en el Recién Nacido/ Cardiopatías neonatales",
+        url: "http://link-al-video.com",
+    },
 ];
 
 // const path = "cursos/6ThlcAF2z98yAXyJ4xT1/Modulos/yI2XjQ4swxT5NxTcDc3B/items";
 // agregarItemsModulo(path, itemsToBeAdded);
 
-export {
-  CursosInscriptos,
-  collection,
-  db,
-  getDoc,
-  doc,
-  addDoc,
-  setDoc,
-  updateDoc,
-  getDocs,
-  query,
-  where,
-  ContenidoXCurso,
-};
-
-const ContenidoXCurso = async (cursoid) => {
-  const contenidoRef = collection(db, "contenido");
-  const q1 = query(contenidoRef, where("cursoid", "==", cursoid));
-
-  const querySnapshot = await getDocs(q1);
-
-  // Construye un array con los resultados
-  const resultados = [];
-
-  querySnapshot.forEach((doc) => {
-    resultados.push({ id: doc.id, ...doc.data() });
-  });
-
-  return resultados;
-};
+export { CursosInscriptos, collection, db, getDoc, doc, addDoc, setDoc, updateDoc, getDocs, query, where };
