115930d60a10c9e0cfb66e6747649fe190b6c229
ctrl1758 <franco.more1758@gmail.com>
2024-06-27 22:52:53 -0300

    mobile card curso

--- src/components/CardCurso.jsx
+++ src/components/CardCurso.jsx
@@ -6,34 +6,38 @@ import { Link } from "react-router-dom";
 import { Button } from "@mui/material";
 
 function CardCurso({ item }) {
-    return (
-        <Link to={`/inscripcion/${item.id}`} className="">
-            <div className="curso-card ">
-                <div className="d-flex align-items-center   justify-content-center ">
-                    <h3 className=" curso-card-title row   ">{item.title}</h3>
-                    <img className="curso-card-img row  " src={item.imageUrl} alt={item.title} />
-                </div>
-                <div className="curso-card-body">
-                    <p className="curso-card-author">
-                        <strong>Modalidad:</strong> {item.modalidad}
-                    </p>
-                    <p className="curso-card-start">
-                        <strong>Inicio:</strong> {item.start}
-                    </p>
-                    <p className="curso-card-duration">
-                        <strong>Duración:</strong> {item.duration}
-                    </p>
-                    <p className="curso-card-start">
-                        <strong>Carga horaria:</strong> {item.workload} hs.
-                    </p>
-                    <p className="curso-card-start">
-                        <strong>Clases:</strong> {item.classes} hs.
-                    </p>
-                    <button className="curso-btn">VER MÁS</button>
-                </div>
-            </div>
-        </Link>
-    );
+  return (
+    <Link to={`/inscripcion/${item.id}`} className="">
+      <div className="curso-card ">
+        <div className="d-flex align-items-center   justify-content-center header-curso">
+          <h3 className=" curso-card-title row   ">{item.title}</h3>
+          <img
+            className="curso-card-img row  "
+            src={item.imageUrl}
+            alt={item.title}
+          />
+        </div>
+        <div className="curso-card-body">
+          <p className="curso-card-author">
+            <strong>Modalidad:</strong> {item.modalidad}
+          </p>
+          <p className="curso-card-start">
+            <strong>Inicio:</strong> {item.start}
+          </p>
+          <p className="curso-card-duration">
+            <strong>Duración:</strong> {item.duration}
+          </p>
+          <p className="curso-card-start">
+            <strong>Carga horaria:</strong> {item.workload} hs.
+          </p>
+          <p className="curso-card-start">
+            <strong>Clases:</strong> {item.classes} hs.
+          </p>
+          <button className="curso-btn">VER MÁS</button>
+        </div>
+      </div>
+    </Link>
+  );
 }
 
 export default CardCurso;
