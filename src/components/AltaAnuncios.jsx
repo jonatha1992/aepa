d69351b8734fa37378fdba3f923a33e6572ecab4
jonatha1992 <jonathancorrea291292@hotmail.com>
2024-06-28 00:44:07 -0300

    falta colocar las imagenes para los anuncios y colocar el toast cuando se agrega el evento y anuncio , se coloco el card de vista previa para visualizacion del card

--- src/components/AltaAnuncios.jsx
+++ src/components/AltaAnuncios.jsx
@@ -1,86 +1,81 @@
 import React, { useState } from "react";
 import { Button, TextField, Box } from "@mui/material";
 import { agregarDoc } from "../firebase";
+import CardEventoVistaPrevia from "./CardEventoVistaPrevia";
 
 const AltaAnuncios = () => {
-  const [formValues, setFormValues] = useState({
-    TITULO: "",
-    SUBTITULO: "",
-    DESCRIPCION: "",
-  });
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormValues({
-      ...formValues,
-      [name]: value,
+    const [formValues, setFormValues] = useState({
+        TITULO: "",
+        SUBTITULO: "",
+        DESCRIPCION: "",
     });
-  };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Aquí puedes manejar el envío del formulario, por ejemplo, enviarlo a una API
-    agregarDoc(formValues, "anuncios")
-      .then((ID) => {
-        console.log("Documento escrito con ID: ", ID);
-      })
-      .catch((error) => {
-        console.error("Error al agregar el documento: ", error);
-      });
-    console.log("Anuncio creado:", formValues);
-    // Reiniciar el formulario después de enviarlo
-    setFormValues({
-      TITULO: "",
-      SUBTITULO: "",
-      DESCRIPCION: "",
-    });
-  };
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormValues({
+            ...formValues,
+            [name]: value,
+        });
+    };
 
-  return (
-    <Box
-      component="form"
-      onSubmit={handleSubmit}
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        gap: "1rem",
-        maxWidth: "500px",
-        margin: "0 auto",
-        padding: "2rem",
-        backgroundColor: "white",
-        borderRadius: "8px",
-        boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
-      }}
-    >
-      <h2>Alta de Anuncios</h2>
-      <TextField
-        label="Título"
-        name="TITULO"
-        value={formValues.TITULO}
-        onChange={handleInputChange}
-        required
-      />
-      <TextField
-        label="Subtítulo"
-        name="SUBTITULO"
-        value={formValues.SUBTITULO}
-        onChange={handleInputChange}
-        required
-      />
-      <TextField
-        label="Descripción"
-        name="DESCRIPCION"
-        value={formValues.DESCRIPCION}
-        onChange={handleInputChange}
-        required
-        multiline
-        rows={4}
-      />
-      <Button type="submit" variant="contained" color="primary">
-        Crear Anuncio
-      </Button>
-    </Box>
-  );
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Aquí puedes manejar el envío del formulario, por ejemplo, enviarlo a una API
+        agregarDoc(formValues, "anuncios")
+            .then((ID) => {
+                console.log("Documento escrito con ID: ", ID);
+            })
+            .catch((error) => {
+                console.error("Error al agregar el documento: ", error);
+            });
+        console.log("Anuncio creado:", formValues);
+        // Reiniciar el formulario después de enviarlo
+        setFormValues({
+            TITULO: "",
+            SUBTITULO: "",
+            DESCRIPCION: "",
+        });
+    };
+
+    return (
+        <div className="d-flex">
+            <Box
+                component="form"
+                onSubmit={handleSubmit}
+                sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "1rem",
+                    maxWidth: "500px",
+                    margin: "0 auto",
+                    padding: "2rem",
+                    backgroundColor: "white",
+                    borderRadius: "8px",
+                    marginTop: "2rem",
+
+                    boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
+                }}
+                className="col-5"
+            >
+                <h2>Alta de Anuncios</h2>
+                <TextField label="Título" name="TITULO" value={formValues.TITULO} onChange={handleInputChange} required />
+                <TextField label="Subtítulo" name="SUBTITULO" value={formValues.SUBTITULO} onChange={handleInputChange} required />
+                <TextField
+                    label="Descripción"
+                    name="DESCRIPCION"
+                    value={formValues.DESCRIPCION}
+                    onChange={handleInputChange}
+                    required
+                    multiline
+                    rows={4}
+                />
+                <Button type="submit" variant="contained" color="primary">
+                    Crear Anuncio
+                </Button>
+            </Box>
+            <CardEventoVistaPrevia titulo={formValues.TITULO} subTitulo={formValues.SUBTITULO} descripcion={formValues.DESCRIPCION} />
+        </div>
+    );
 };
 
 export default AltaAnuncios;
---
+++ src/components/CardEventoVistaPrevia.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { FaWhatsapp, FaFacebook, FaInstagram } from "react-icons/fa";
+import { FiMail } from "react-icons/fi";
+const defaultImageURL = "https://www.uba.ar/imgs/w-HOSPITALES-1920x980.jpg";
+
+const CardEventoVistaPrevia = ({ titulo, subTitulo, descripcion }) => {
+    const divStyle = {
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        height: "200px",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        color: "#ffffff",
+        textAlign: "center",
+        flexDirection: "column",
+    };
+
+    return (
+        <div className="col-4 card-evento-anuncio  mt-5 mx-5" style={{ height: "70vh" }}>
+            <div className="card-event-image" style={{ ...divStyle, backgroundImage: `url(${defaultImageURL})` }}>
+                <h3 className="card-evento-titulo">{titulo}</h3>
+                <div className="d-flex justify-content-end" style={{ width: "100%" }}>
+                    <p className="card-evento-fecha">{subTitulo}</p>
+                </div>
+            </div>
+            <div className="d-flex flex-column justify-content-around" style={{ flexGrow: "1", textAlign: "center", height: "60%" }}>
+                <div className="body-card-seccionanuncios">
+                    <p className="card-evento-descripcion">{descripcion}</p>
+                </div>
+            </div>
+            <div className="d-flex justify-content-center flex-row pt-4">
+                <a style={{ color: "#516269" }}>
+                    <FaWhatsapp size={25} className="me-3" />
+                </a>
+                <a style={{ color: "#516269" }}>
+                    <FiMail size={25} className="me-3" />
+                </a>
+                <a style={{ color: "#516269" }}>
+                    <FaFacebook size={25} className="me-3" />
+                </a>
+                <a style={{ color: "#516269" }}>
+                    <FaInstagram size={25} className="me-3" />
+                </a>
+                <a style={{ color: "#516269" }}>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="25"
+                        height="25"
+                        fill="currentColor"
+                        className="bi bi-tiktok"
+                        viewBox="0 0 16 16"
+                    >
+                        <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z" />
+                    </svg>
+                </a>
+            </div>
+        </div>
+    );
+};
+
+export default CardEventoVistaPrevia;
