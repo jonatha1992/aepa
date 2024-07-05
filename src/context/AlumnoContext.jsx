a7e280a2a7f96300a374e8458ed096112dbb2712
ctrl1758 <franco.more1758@gmail.com>
2024-07-05 00:49:01 -0300

    bug fix MisCursos

--- src/context/AlumnoContext.jsx
+++ src/context/AlumnoContext.jsx
@@ -5,39 +5,42 @@ import { CursosInscriptos, CursosAdmin } from "../firebase.js";
 export const AlumnosContext = createContext();
 
 export function AlumnosContextProvider(props) {
-    const { User } = useAuth();
-    const [cursos, setCursos] = useState([]);
-    const [activeCourse, setActiveCourse] = useState(null);
+  const { User } = useAuth();
+  const [cursos, setCursos] = useState([]);
+  const [activeCourse, setActiveCourse] = useState(null);
 
-    useEffect(() => {
-        const fetchDataFromFirebase = async () => {
-            try {
-                let miscursos = [];
-                if (User.rol === "admin") {
-                    miscursos = await CursosAdmin();
-                } else {
-                    miscursos = await CursosInscriptos(User.uid);
-                }
-                setCursos(miscursos);
-                console.log("cursos:", miscursos);
-            } catch (error) {
-                console.error("Error al obtener datos de Firebase: necesita logearse primero");
-            }
-        };
+  useEffect(() => {
+    const fetchDataFromFirebase = async () => {
+      try {
+        let miscursos = [];
+        if (User.rol === "admin") {
+          miscursos = await CursosAdmin();
+        } else {
+          miscursos = await CursosInscriptos(User.uid);
+          console.log("misCursos:", miscursos);
+        }
+        setCursos(miscursos);
+        console.log("cursos:", miscursos);
+      } catch (error) {
+        console.error(
+          "Error al obtener datos de Firebase: necesita logearse primero"
+        );
+      }
+    };
 
-        fetchDataFromFirebase();
-    }, [User]);
-    return (
-        <>
-            <AlumnosContext.Provider
-                value={{
-                    cursos,
-                    setActiveCourse,
-                    activeCourse,
-                }}
-            >
-                {props.children}
-            </AlumnosContext.Provider>
-        </>
-    );
+    fetchDataFromFirebase();
+  }, [User]);
+  return (
+    <>
+      <AlumnosContext.Provider
+        value={{
+          cursos,
+          setActiveCourse,
+          activeCourse,
+        }}
+      >
+        {props.children}
+      </AlumnosContext.Provider>
+    </>
+  );
 }
