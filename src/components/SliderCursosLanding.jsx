4342da61100cbbdbac19448eaeb1761716bb43b9
jonatha1992 <jonathancorrea291292@hotmail.com>
2024-07-05 22:21:03 -0300

    cambios en slider cursos

--- src/components/SliderCursosLanding.jsx
+++ src/components/SliderCursosLanding.jsx
@@ -20,30 +20,30 @@ function SliderCursosLanding({ cursos }) {
     );
 
     const array = cursos.cursos;
-    const hay1Curso = array && array.length === 1;
+    const cursosCount = array ? array.length : 0;
 
     const settings = {
         dots: false,
-        infinite: true,
+        infinite: cursosCount > 2,
         speed: 500,
-        slidesToShow: hay1Curso ? 1 : 3,
-        slidesToScroll: hay1Curso ? 1 : 2,
+        slidesToShow: cursosCount === 2 ? 2 : 3,
+        slidesToScroll: cursosCount === 2 ? 2 : 2,
         initialSlide: 0,
         responsive: [
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: hay1Curso ? 1 : 3,
-                    slidesToScroll: hay1Curso ? 1 : 2,
-                    infinite: true,
+                    slidesToShow: cursosCount === 2 ? 2 : 3,
+                    slidesToScroll: cursosCount === 2 ? 2 : 2,
+                    infinite: cursosCount > 2,
                 },
             },
             {
                 breakpoint: 900,
                 settings: {
-                    slidesToShow: hay1Curso ? 1 : 2,
-                    slidesToScroll: hay1Curso ? 1 : 2,
-                    initialSlide: hay1Curso ? 0 : 2,
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2,
                 },
             },
             {
@@ -54,11 +54,11 @@ function SliderCursosLanding({ cursos }) {
                 },
             },
         ],
-        prevArrow: <CustomPrevArrow />,
-        nextArrow: <CustomNextArrow />,
+        // prevArrow: <CustomPrevArrow />,
+        // nextArrow: <CustomNextArrow />,
     };
 
-    if (hay1Curso) {
+    if (cursosCount === 1) {
         return (
             <div className="slider-container d-flex justify-content-center">
                 <div className="col-12 col-md-4">
@@ -70,10 +70,12 @@ function SliderCursosLanding({ cursos }) {
 
     return (
         <div className="slider-container d-flex justify-content-center">
-            <div className="col-12">
+            <div className={`col-12 ${cursosCount === 2 ? "col-md-8" : ""}`}>
                 <Slider {...settings}>
                     {array.map((item, index) => (
-                        <CardCurso key={index} item={item} />
+                        <div key={index} className={`px-2 ${cursosCount === 2 ? "col-md-6" : ""}`}>
+                            <CardCurso item={item} />
+                        </div>
                     ))}
                 </Slider>
             </div>
