e2d8d71098503f08bced7fed80da64be7bb0d7e3
jonatha1992 <jonathancorrea291292@hotmail.com>
2024-07-05 21:09:53 -0300

    sacado de icono de  notificaciones en mobile

--- src/components/DashbordAlumnos.jsx
+++ src/components/DashbordAlumnos.jsx
@@ -49,64 +49,56 @@
 import React from "react";
 import { useAuth } from "../context/AuthContext";
 import { Link } from "react-router-dom";
-import {
-  AutoStories,
-  Home,
-  Mail,
-  Settings,
-  Business,
-} from "@mui/icons-material"; // Importa el nuevo ícono para el feature de administrador
+import { AutoStories, Home, Mail, Settings, Business } from "@mui/icons-material"; // Importa el nuevo ícono para el feature de administrador
 
 import "../css/dashbordAlumnos.css";
 
 const FeatureGrid = () => {
-  const { User } = useAuth();
+    const { User } = useAuth();
 
-  const isAdmin = User.rol === "admin"; // Verifica si el usuario es administrador
+    const isAdmin = User.rol === "admin"; // Verifica si el usuario es administrador
 
-  // Define los features comunes para todos los usuarios
-  const features = [
-    {
-      name: "Mis Cursos",
-      icon: <AutoStories fontSize="large" />,
-      route: "/miscursos",
-    },
-    { name: "Perfil", icon: <Settings fontSize="large" />, route: "/perfil" },
-    {
-      name: "Notificaciones",
-      icon: <Mail fontSize="large" />,
-      route: "/notificaciones",
-    },
-    { name: "Home", icon: <Home fontSize="large" />, route: "/" },
-  ];
+    // Define los features comunes para todos los usuarios
+    const features = [
+        {
+            name: "Mis Cursos",
+            icon: <AutoStories fontSize="large" />,
+            route: "/miscursos",
+        },
+        { name: "Perfil", icon: <Settings fontSize="large" />, route: "/perfil" },
+        // {
+        //   name: "Notificaciones",
+        //   icon: <Mail fontSize="large" />,
+        //   route: "/notificaciones",
+        // },
+        { name: "Home", icon: <Home fontSize="large" />, route: "/" },
+    ];
 
-  // Si el usuario es administrador, agrega un feature adicional
-  if (isAdmin) {
-    features.push({
-      name: "Administrar",
-      icon: <Business fontSize="large" />,
-      route: "/admin",
-    });
-  }
+    // Si el usuario es administrador, agrega un feature adicional
+    if (isAdmin) {
+        features.push({
+            name: "Administrar",
+            icon: <Business fontSize="large" />,
+            route: "/admin",
+        });
+    }
 
-  return (
-    <div className="container" style={{ paddingTop: "10rem" }}>
-      <h3 style={{ textAlign: "start", color: "#606468" }}>
-        Bienvenido {User.nombre_completo}.
-      </h3>
+    return (
+        <div className="container" style={{ paddingTop: "10rem" }}>
+            <h3 style={{ textAlign: "start", color: "#606468" }}>Bienvenido {User.nombre_completo}.</h3>
 
-      <div className="feature-grid">
-        {features.map((feature, index) => (
-          <Link to={feature.route} key={index}>
-            <div className="feature-item">
-              {feature.icon}
-              <span>{feature.name}</span>
+            <div className="feature-grid">
+                {features.map((feature, index) => (
+                    <Link to={feature.route} key={index}>
+                        <div className="feature-item">
+                            {feature.icon}
+                            <span>{feature.name}</span>
+                        </div>
+                    </Link>
+                ))}
             </div>
-          </Link>
-        ))}
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default FeatureGrid;
