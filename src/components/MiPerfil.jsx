b90892928a35ceb111425cac5373fbef5f674070
jonathan correa <jonathancorrea291292@hotmail.com>
2024-07-14 23:38:59 -0300

    colocar backfrop en cursos y mi perfil

--- src/components/MiPerfil.jsx
+++ src/components/MiPerfil.jsx
@@ -1,80 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useAuth } from "../context/AuthContext";
-import { FaUserCircle } from "react-icons/fa"; // Importa react-icons para usar iconos
-import { Height } from "@mui/icons-material";
+import { FaUserCircle } from "react-icons/fa";
+import { Backdrop, CircularProgress } from "@mui/material";
 import Breadcrumbs from "./Breadcrumbs";
 
 export default function MiPerfil() {
-  const { User } = useAuth();
+    const { User } = useAuth();
+    const [loading, setLoading] = useState(true);
 
-  // Comprueba si hay un usuario antes de intentar acceder a sus propiedades
-  if (!User) {
+    useEffect(() => {
+        const simulateLoading = async () => {
+            setLoading(true);
+            // Simular una carga de 2 segundos
+            await new Promise((resolve) => setTimeout(resolve, 1500));
+            setLoading(false);
+        };
+
+        simulateLoading();
+    }, []);
+
+    if (loading || !User) {
+        return (
+            <Backdrop open={true} style={{ zIndex: 9999, color: "#fff" }}>
+                <CircularProgress color="inherit" />
+            </Backdrop>
+        );
+    }
+
     return (
-      <div className="text-center mt-5">
-        <div className="spinner-border text-primary" role="status">
-          <span className="sr-only">Cargando...</span>
-        </div>
-      </div>
-    );
-  }
-
-  return (
-    <div className="container ">
-      <div
-        className=" align-items-center contenido-container d-flex justify-content-start flex-column"
-        style={{}}
-      >
-        <Breadcrumbs />
-        <div className="card border-0 shadow" style={{ width: "25rem" }}>
-          <div className="card-body">
-            <div className="text-center mb-3">
-              <FaUserCircle size="100px" className="text-secondary" />
+        <div className="container">
+            <div className="align-items-center contenido-container d-flex justify-content-start flex-column">
+                <Breadcrumbs />
+                <div className="card border-0 shadow" style={{ width: "25rem" }}>
+                    <div className="card-body">
+                        <div className="text-center mb-3">
+                            <FaUserCircle size="100px" className="text-secondary" />
+                        </div>
+                        <h4 className="card-title text-center mb-3">{User.nombre_completo}</h4>
+                        <h6 className="text-secondary fw-bold mb-2">
+                            Rol:
+                            <strong style={{ color: "var(--color3)" }} className="text-uppercase">
+                                {" " + User.rol}
+                            </strong>
+                        </h6>
+                        <h6 className="text-secondary fw-bold mb-2">
+                            Email:
+                            <strong style={{ color: "var(--color3)" }}>{" " + User.email}</strong>
+                        </h6>
+                        <h6 className="text-secondary fw-bold mb-2">
+                            DNI:
+                            <strong style={{ color: "var(--color3)" }}>{" " + User.DNI}</strong>
+                        </h6>
+                        <h6 className="text-secondary fw-bold mb-2">
+                            Localidad:
+                            <strong style={{ color: "var(--color3)" }}>{" " + User.localidad}</strong>
+                        </h6>
+                        <h6 className="text-secondary fw-bold mb-2">
+                            Telefono:
+                            <strong style={{ color: "var(--color3)" }}>{" " + User.telefono}</strong>
+                        </h6>
+                        <h6 className="text-secondary fw-bold mb-2">
+                            Nivel:
+                            <strong style={{ color: "var(--color3)" }}>{" " + User.nivel}</strong>
+                        </h6>
+                    </div>
+                </div>
             </div>
-            <h4 className="card-title text-center mb-3">
-              {User.nombre_completo}
-            </h4>
-            <h6 className=" text-secondary fw-bold  mb-2">
-              Rol:
-              <strong
-                style={{ color: "var(--color3)" }}
-                className="text-uppercase "
-              >
-                {" " + User.rol}
-              </strong>
-            </h6>
-            <h6 className="text-secondary fw-bold  mb-2">
-              Email:
-              <strong style={{ color: "var(--color3)" }}>
-                {" " + User.email}
-              </strong>
-            </h6>
-            <h6 className="text-secondary fw-bold  mb-2">
-              DNI:
-              <strong style={{ color: "var(--color3)" }}>
-                {" " + User.DNI}
-              </strong>
-            </h6>
-            <h6 className=" text-secondary fw-bold  mb-2">
-              Localidad:
-              <strong style={{ color: "var(--color3)" }}>
-                {" " + User.localidad}
-              </strong>
-            </h6>
-            <h6 className="text-secondary fw-bold  mb-2">
-              Telefono:
-              <strong style={{ color: "var(--color3)" }}>
-                {" " + User.telefono}
-              </strong>
-            </h6>
-            <h6 className="text-secondary fw-bold  mb-2">
-              Nivel:
-              <strong style={{ color: "var(--color3)" }}>
-                {" " + User.nivel}
-              </strong>
-            </h6>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
--- src/components/MisCursos.jsx
+++ src/components/MisCursos.jsx
@@ -5,91 +5,102 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import UnidadesCursos from "./UnidadesCursos";
 import Breadcrumbs from "./Breadcrumbs";
 import { Link } from "react-router-dom";
+import { Backdrop, CircularProgress } from "@mui/material";
 
 const MisCursos = () => {
-  const { cursos, activeCourse, setActiveCourse } = useContext(AlumnosContext);
-  const handleCursoClick = (curso) => {
-    setActiveCourse(curso);
-  };
+    const { cursos, activeCourse, setActiveCourse } = useContext(AlumnosContext);
+    const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    setActiveCourse(null);
-    window.scrollTo(0, 0);
-  }, []);
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+    const handleCursoClick = (curso) => {
+        setActiveCourse(curso);
+    };
 
-  return (
-    <div className="contenido-container">
-      <Breadcrumbs />
-      {cursos.length === 0 ? (
-        <div>
-          <div className="no-cursos-message">
-            <p>No estás inscrito en ningún curso. inscribite a uno!</p>
-            <Link to="/#seccion3" className="text-primary hover-primary ">
-              Ver cursos
-            </Link>
-          </div>
-        </div>
-      ) : (
-        <div className=" d-flex justify-content-start flex-column">
-          {activeCourse ? (
-            <UnidadesCursos activeCourse={activeCourse} />
-          ) : (
-            <div
-              className="container d-flex flex-column"
-              style={{ height: "100vh" }}
-            >
-              <div
-                className="container-miscursos"
-                style={{ color: "black", paddingTop: "" }}
-              >
-                {cursos.map((curso, index) => (
-                  <div
-                    key={index}
-                    className="miscursos-item"
-                    onClick={() => handleCursoClick(curso)}
-                  >
-                    <div
-                      className="blur-background"
-                      style={{
-                        backgroundImage: `url(${curso.detalles.imageUrl})`,
-                      }}
-                    >
-                      <div className="content d-flex">
-                        <span
-                          style={{
-                            width: "70%",
-                            fontSize: "1rem",
-                            textAlign: "start",
-                            color: "#606468",
-                            fontWeight: "bold",
-                          }}
-                        >
-                          {curso.detalles.title}
-                        </span>
-                        <div
-                          className="info-micurso"
-                          style={{
-                            width: "30%",
-                            textAlign: "end",
-                            fontSize: "10px",
-                          }}
-                        >
-                          <span>{curso.detalles.modulosCount} Modulos</span>
-                        </div>
-                      </div>
+    useEffect(() => {
+        const simulateLoading = async () => {
+            console.log("Inicio de la carga simulada");
+            setLoading(true);
+            // Simular una carga de 2 segundos
+            await new Promise((resolve) => setTimeout(resolve, 1500));
+            console.log("Fin de la carga simulada");
+            setLoading(false);
+            setActiveCourse(null);
+            window.scrollTo(0, 0);
+        };
+
+        simulateLoading();
+    }, []);
+
+    if (loading) {
+        console.log("Renderizando Backdrop");
+        return (
+            <Backdrop open={true} style={{ zIndex: 9999, color: "#fff" }}>
+                <CircularProgress color="inherit" />
+            </Backdrop>
+        );
+    }
+
+    console.log("Renderizando contenido principal");
+
+    return (
+        <div className="contenido-container">
+            <Breadcrumbs />
+            {cursos.length === 0 ? (
+                <div>
+                    <div className="no-cursos-message">
+                        <p>No estás inscrito en ningún curso. ¡Inscríbete a uno!</p>
+                        <Link to="/#seccion3" className="text-primary hover-primary ">
+                            Ver cursos
+                        </Link>
                     </div>
-                  </div>
-                ))}
-              </div>
-            </div>
-          )}
+                </div>
+            ) : (
+                <div className="d-flex justify-content-start flex-column">
+                    {activeCourse ? (
+                        <UnidadesCursos activeCourse={activeCourse} />
+                    ) : (
+                        <div className="container d-flex flex-column" style={{ height: "100vh" }}>
+                            <div className="container-miscursos" style={{ color: "black", paddingTop: "" }}>
+                                {cursos.map((curso, index) => (
+                                    <div key={index} className="miscursos-item" onClick={() => handleCursoClick(curso)}>
+                                        <div
+                                            className="blur-background"
+                                            style={{
+                                                backgroundImage: `url(${curso.detalles.imageUrl})`,
+                                            }}
+                                        >
+                                            <div className="content d-flex">
+                                                <span
+                                                    style={{
+                                                        width: "70%",
+                                                        fontSize: "1rem",
+                                                        textAlign: "start",
+                                                        color: "#606468",
+                                                        fontWeight: "bold",
+                                                    }}
+                                                >
+                                                    {curso.detalles.title}
+                                                </span>
+                                                <div
+                                                    className="info-micurso"
+                                                    style={{
+                                                        width: "30%",
+                                                        textAlign: "end",
+                                                        fontSize: "10px",
+                                                    }}
+                                                >
+                                                    <span>{curso.detalles.modulosCount} Módulos</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    )}
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default MisCursos;
